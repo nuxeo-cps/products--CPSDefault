a6b85554194443294fa7734ec0339fc785181c7e
bdelbosc <devnull@localhost>
2003-05-13 15:27:56 +0000

    default sort is folderish status then title

--- skins/cps_default/getFolderContents.py
+++ skins/cps_default/getFolderContents.py
@@ -6,21 +6,33 @@
 Get a datastructure describing what's in a folder.
 """
 
-mtool=context.portal_membership;
+mtool=context.portal_membership
+wtool=context.portal_workflow
 fc = []
-for item in context.listFolderContents(contentFilter=contentFilter):
+bmt = context.Benchmarktimer()
+bmt.setMarker('folder')
+for item in context.objectValues():
     if item.getId().startswith('.'):
         continue
     if not mtool.checkPermission('View', item):
         continue
     fc.append(item)
 
-def thecmp(a, b): # cmp by folder then title
+def title_cmp(a, b): # cmp by folder then title
     return (-cmp(a.isPrincipiaFolderish, b.isPrincipiaFolderish) or
             cmp(a.title_or_id().lower(), b.title_or_id().lower()) or
             0
             )
 
-fc.sort(thecmp)
+def status_cmp(a, b):
+    return (-cmp(a.isPrincipiaFolderish, b.isPrincipiaFolderish) or
+            cmp(wtool.getInfoFor(a, 'review_state', ''),
+                wtool.getInfoFor(b, 'review_state', '')) or
+            cmp(a.title_or_id().lower(), b.title_or_id().lower()) or
+            0
+            )
+
+bmt.setMarker('folder_')
+fc.sort(status_cmp)
 
 return fc

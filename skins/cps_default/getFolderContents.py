5ba65980af9d69bdf91716194f8261949c33f082
bdelbosc <devnull@localhost>
2003-05-15 10:43:26 +0000

    optimize sorting 4 time faster

--- skins/cps_default/getFolderContents.py
+++ skins/cps_default/getFolderContents.py
@@ -1,15 +1,17 @@
 ## Script (Python) "getFolderContents"
-##parameters=contentFilter=None
+##parameters=sort_by='title', direction='asc'
 ##title=
 ##
 """
-Get a datastructure describing what's in a folder.
+Get a sorted list of contents object
 """
 bmt = context.Benchmarktimer('getFolderContent', level=-2)
 bmt.start()
 
 mtool=context.portal_membership
 wtool=context.portal_workflow
+
+# filtering
 items = []
 now = context.ZopeTime()
 for item in context.objectValues():
@@ -20,32 +22,49 @@ for item in context.objectValues():
     if item.effective() <= now and item.expires() > now:
         items.append(item)
 
-def simple_cmp(a, b):
-    return (cmp(a.getId(), b.getId()))
+# sorting
+# XXX hardcoded status !
+status_sort_order={'nostate':'0',
+                   'pending':'1',
+                   'published':'2',
+                   'work':'3',
+                   }
 
-def title_cmp(a, b): # cmp by folder then title
-    return (-cmp(a.isPrincipiaFolderish, b.isPrincipiaFolderish) or
-            cmp(a.title_or_id().lower(), b.title_or_id().lower()) or
-            0
-            )
+def id_sortkey(a):
+    return str(not a.isPrincipiaFolderish) + \
+           a.getId()
 
-def status_cmp(a, b): # cmp by folder, status then title
-    return (-cmp(a.isPrincipiaFolderish, b.isPrincipiaFolderish) or
-            cmp(wtool.getInfoFor(a, 'review_state', ''),
-                wtool.getInfoFor(b, 'review_state', '')) or
-            cmp(a.title_or_id().lower(), b.title_or_id().lower()) or
-            0
-            )
+def status_sortkey(a):
+    return str(not a.isPrincipiaFolderish) + \
+           status_sort_order[wtool.getInfoFor(a,'review_state','nostate')] + \
+           a.title_or_id().lower()
 
-def date_cmp(a, b): # cmp by folder, date then title
-    return (-cmp(a.isPrincipiaFolderish, b.isPrincipiaFolderish) or
-            -cmp(wtool.getInfoFor(a, 'time', ''),
-                 wtool.getInfoFor(b, 'time', '')) or
-            cmp(a.title_or_id().lower(), b.title_or_id().lower()) or
-            0
-            )
+def title_sortkey(a):
+    return str(not a.isPrincipiaFolderish) + \
+           a.title_or_id().lower()
 
-items.sort(status_cmp)
+def date_sortkey(a):
+    return str(not a.isPrincipiaFolderish) + \
+           str(wtool.getInfoFor(a,'time','x').timeTime()) + \
+           a.getId()
+
+def cmp_desc(x, y):
+    return -cmp(x, y)
+
+make_sortkey = id_sortkey
+if sort_by == 'status':
+    make_sortkey = status_sortkey
+elif sort_by == 'date':
+    make_sortkey = status_sortkey
+elif sort_by == 'title':
+    make_sortkey = title_sortkey
+    
+objects = [ ( make_sortkey(x), x ) for x in items ]
+if direction != 'asc':
+    objects.sort(cmp_desc)
+else:
+    objects.sort() # tuples compare "lexicographically"
+items = [ x[1] for x in objects ]
 
 bmt.stop()
 bmt.saveProfile(context.REQUEST)

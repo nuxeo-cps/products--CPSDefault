07099a86141b814af819b70f842dfb020dc761f7
bdelbosc <devnull@localhost>
2003-05-20 08:57:12 +0000

    adding default arg

--- skins/cps_default/getDocumentActions.py
+++ skins/cps_default/getDocumentActions.py
@@ -1,4 +1,7 @@
-##parameters=actions
+##parameters=actions=None
+
+if not actions:
+    actions=context.portal_actions.listFilteredActionsFor(context);
 
 actionsblocks = filter(None, [actions['workflow'],
                               actions['object'],

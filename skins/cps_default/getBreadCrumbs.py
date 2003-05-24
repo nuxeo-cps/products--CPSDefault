d86ff495df79fb4a951371da20c97b2c2e419e28
hcauwelier <devnull@localhost>
2003-05-24 13:14:21 +0000

    correction du bug "oops i did it again !" du breadcrumbs

--- skins/cps_default/getBreadCrumbs.py
+++ skins/cps_default/getBreadCrumbs.py
@@ -5,19 +5,21 @@
 from string import join
 
 result = []
-portal_url = context.portal_url(relative=1) + '/'
+portal_url = context.portal_url
+rurl = portal_url(relative=1)
+rurl = rurl and '/%s/' % rurl or ''
 
 if include_root:
     title = context.portal_properties.title().strip()
     result.append( { 'id': 'root'
                    , 'title': (len(title) > 15) and title[:15] + '...' or title
                    , 'longtitle': title
-                   , 'url': portal_url
+                   , 'url': rurl
                    }
                  )
 
-relative = context.portal_url.getRelativeContentPath(context)
-portal = context.portal_url.getPortalObject()
+relative = portal_url.getRelativeContentPath(context)
+portal = portal_url.getPortalObject()
 
 for i in range(len(relative)):
     now = relative[:i+1]
@@ -27,7 +29,7 @@ for i in range(len(relative)):
         result.append( { 'id': now[-1]
                        , 'title': (len(title) > 12) and title[:12] + '...' or title
                        , 'longtitle': title
-                       , 'url': portal_url + join(now, '/') + '/'
+                       , 'url': rurl + join(now, '/') + '/'
                        }
                     )
 

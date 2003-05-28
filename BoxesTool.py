85f26d04e665c04384eec7dd1885cb6ee8919b5f
lregebro <devnull@localhost>
2003-05-28 15:48:03 +0000

    Added personal boxes support. The localized settings 'overrides' are now stored in the _box_overrides attribute of the .cps_boxes object. It is a PersistentMapping. This makes it kinda hard to manage at the moment, since their is no skins for it.

--- BoxesTool.py
+++ BoxesTool.py
@@ -3,7 +3,7 @@
 """
   BoxesTool
 """
-import string
+from zLOG import LOG, DEBUG
 from DateTime import DateTime
 from Globals import InitializeClass, DTMLFile
 from types import DictType, StringType
@@ -11,11 +11,11 @@ from types import DictType, StringType
 from AccessControl import ClassSecurityInfo
 from Acquisition import aq_base, aq_parent, aq_inner
 from OFS.SimpleItem import SimpleItem
+from ZODB.PersistentMapping import PersistentMapping
 
 from Products.CMFCore.CMFCorePermissions import setDefaultRoles, \
      View, AccessContentsInformation, ManagePortal
 from Products.CMFCore.utils import UniqueObject, getToolByName, _checkPermission
-from zLOG import LOG, DEBUG
 
 
 class BoxesTool(UniqueObject, SimpleItem):
@@ -41,19 +41,14 @@ class BoxesTool(UniqueObject, SimpleItem):
     #
     # Public API
     #
-    #security.declarePublic('getBoxes')
-    #def getBoxes(self, context, slot=None):
-    #    boxesinfo = self.getBoxesInfo(context, slot)
-    #    return [info['box'] for info in boxesinfo]
-
 
     security.declarePublic('getBoxes')
     def getBoxes(self, context, slot=None):
         """Return a sorted list of boxes
         box are loaded from root to current path
         and overriden by personal boxes folder
-        return a list of:
-            [box_id, settings, macro_path, box_object]
+        return a list of dictionaries with keys:
+            'box_url', 'settings', 'macro', 'box'
         """
 
         # Find bottom-most folder:
@@ -74,7 +69,7 @@ class BoxesTool(UniqueObject, SimpleItem):
         portal_url = getToolByName(self, 'portal_url')
         rpath = portal_url.getRelativeContentPath(bmf)
         obj = portal_url.getPortalObject()
-        boxes = []
+        allboxes = []
         settings = {}
         path = '/'
 
@@ -83,38 +78,48 @@ class BoxesTool(UniqueObject, SimpleItem):
                 path += elem + '/'
                 obj = getattr(obj, elem)
             f_boxes, f_settings = self._getFolderBoxesAndSettings(obj)
-            if len(f_boxes):
-                LOG('portal_boxes', DEBUG, '.cps_boxes found in %s' % path)
-            if len(f_settings):
-                LOG('portal_boxes', DEBUG, 'got settings in %s: %s' % (
-                    path, str(f_settings)))
-
-            for box in f_boxes:
-                # Skip it if there is no view permission
-                if not _checkPermission('View', box):
-                    continue
-                # boxes should be visible if they are to be displayed
-                # in the subfolder, or if this is the root or if
-                # this is the last part of the path.
-                if box.display_in_subfolder or not rpath or elem == rpath[-1]:
-                    boxes.append({'path': portal_url.getRelativeUrl(box),
-                                'settings': box.getSettings(),
-                                'macro': box.getMacro(),
-                                'box': box})
+            allboxes.extend(f_boxes)
             self._updateSettings(settings, f_settings)
 
-        # TODO: add get .cps_boxes for member
-        # obj = self.restrictedTraverse( '~/.cps_boxes')
-        # f_boxes, f_settings = self._getFolderBoxesAndSettings(obj)
-        # ...
+        home = context.portal_membership.getHomeFolder()
+        if home:
+            f_boxes, f_settings = self._getFolderBoxesAndSettings(home)
+            allboxes.extend(f_boxes)
+            self._updateSettings(settings, f_settings)
 
-        # final settings
-        for box in boxes:
-            if not box['box'].locked and settings.get(box['path']):
-                box['settings'].update(settings[box['path']])
-                box[macr] = box[box].getMacro(box['settings']['style'])
+        homepath = portal_url.getRelativeContentPath(home)
+        LOG('portal_boxes: home path', DEBUG, homepath)
+        boxes = []
+        for box in allboxes:
+            # Skip it if there is no view permission
+            if not _checkPermission('View', box):
+                continue
+            # Only add boxes if they are to be displayed
+            # in the subfolders, or if this is the root or if
+            # this is the last part of the path, or if it's a
+            # personal box.
+            boxpath = portal_url.getRelativeContentPath(box)
+            LOG('portal_boxes: box path', DEBUG, boxpath)
+            if box.display_in_subfolder or \
+               not rpath or \
+               elem == rpath[-1] or \
+               boxpath[:len(homepath)] == homepath:
+                newbox = {'path': portal_url.getRelativeUrl(box),
+                            'settings': box.getSettings(),
+                            'macro': box.getMacro(),
+                            'box': box}
+                # Override any box settings with the local settings
+                # If the box isn't locked and there are overrides
+                if not newbox['box'].locked and settings.get(newbox['path']):
+                    newbox['settings'].update(settings[newbox['path']])
+                    newbox['macro'] = newbox['box'].getMacro(newbox['settings']['style'])
 
-        # filtering on closed, slot
+                boxes.append(newbox)
+
+        # We now have a list of all boxes that can be displayed in this context.
+        # We'll now filter to only get the open boxes in the asked for slot.
+        # This can not be done previously, since 'slot' and 'closed' settings
+        # can be overriden by local setting
         boxes = [x for x in boxes if (not x['settings']['closed'] and
                                       (slot is None or x['settings']['slot']==slot))]
 
@@ -123,9 +128,9 @@ class BoxesTool(UniqueObject, SimpleItem):
         # sorting
         def cmpbox(a, b):
             return a['settings']['order'] - b['settings']['order']
-
         boxes.sort(cmpbox)
 
+        LOG('portal_boxes', DEBUG, 'Returned boxes:', str(boxes) + '\n')
         return boxes
 
     #
@@ -138,6 +143,7 @@ class BoxesTool(UniqueObject, SimpleItem):
         """
         boxes = []
         settings = {}
+        folder_boxes = None
         if hasattr(aq_base(folder), '.cps_boxes'):
             folder_boxes = getattr(folder, '.cps_boxes')
             for box in folder_boxes.objectValues():
@@ -145,20 +151,16 @@ class BoxesTool(UniqueObject, SimpleItem):
                     continue
                 boxes.append(box)
 
-            if hasattr(aq_base(folder_boxes), 'settings'):
-                folder_settings = getattr(folder_boxes, 'settings')
-                if type(folder_settings) is StringType:
-                    # XXX TODO find another way without eval
-                    # for security reasons
-                    folder_settings = eval(folder_settings)
-                if type(folder_settings) is DictType:
-                    # XXX TODO check integrity of a folder_setting
-                    settings = folder_settings
+            settings = self.getAllBoxOverrides(folder_boxes)
 
+        if folder_boxes:
+            LOG('portal_boxes', DEBUG,
+                'Found boxes and settings at %s:' % folder_boxes.absolute_url(),
+                "Boxes: %s\nSettings: %s\n" % (str(boxes), str(settings)))
         return boxes, settings
 
 
-    security.declarePrivate('_getFolderBoxes')
+    security.declarePrivate('_updateSettings')
     def _updateSettings(self, set1, set2):
         """Update settings 1 with settings 2 """
         if not set2:
@@ -168,6 +170,36 @@ class BoxesTool(UniqueObject, SimpleItem):
                 set1[k].update(set2[k])
             else:
                 set1[k] = set2[k]
+
+    #security.declareProtected('setBoxOverride', )
+    def setBoxOverride(self, boxurl, settings, context):
+        """Allows you to override the box default settings
+
+        boxurl is the relative url of the box (gotten from
+        portal_url.getRelativeUrl(box) )
+
+        settings is a dictionary of the settings and the values
+
+        context is the object where defaults should be stored.
+        """
+        if not hasattr(aq_base(context), '_box_overrides'):
+            context._box_overrides = PersistentMapping()
+
+        # TODO: check which settings you are allowed to change
+        context._box_overrides[boxurl] = settings
+
+    security.declarePublic('getBoxOverride')
+    def getBoxOverride(self, boxurl, context):
+        """Gets the overridden settings for a box"""
+        if not hasattr(aq_base(context), '_box_overrides'):
+            return {}
+        return context._box_overrides.get(boxurl, {})
+
+    security.declarePublic('getAllBoxOverrides')
+    def getAllBoxOverrides(self, context):
+        """Gets all the local overrides"""
+        return getattr(aq_base(context), '_box_overrides', {})
+
 
 InitializeClass(BoxesTool)
 

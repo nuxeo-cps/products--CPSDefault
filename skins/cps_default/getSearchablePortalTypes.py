4ea1598076f1c407719900a19c3fc79124d0e06c
bdelbosc <devnull@localhost>
2003-05-28 08:23:31 +0000

    fixing header and fixing python error

--- skins/cps_default/getSearchablePortalTypes.py
+++ skins/cps_default/getSearchablePortalTypes.py
@@ -1,7 +1,8 @@
-##parameters=only_ids=0
-
-# only_ids is set when you just want a list of portal types
-# (e.g. for search.py)
+## Script (Python) "getSearchablePortalTypes"
+## parameters=only_ids=0
+# $Id$
+""" Only_ids is set when you just want a list of portal types
+(e.g. for search.py) """
 
 ttool = context.portal_types
 
@@ -10,8 +11,8 @@ searchable = []
 for ti in ttool.listTypeInfo():
     if ti.getActionById('issearchabledocument', None):
         if only_ids:
-          searchable.append(ti.getId())
-        else
-          searchable.append({'id': ti.getId(), 'title': ti.Title()})
-
+            searchable.append(ti.getId())
+        else:
+            searchable.append({'id': ti.getId(), 'title': ti.Title()})
+            
 return searchable

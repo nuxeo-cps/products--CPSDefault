3dd55b0774bfd3ea593bf73d63d1a06fc673e884
bdelbosc <devnull@localhost>
2003-06-02 09:44:20 +0000

    fixing short_title

--- skins/cps_default/getFolderInfo.py
+++ skins/cps_default/getFolderInfo.py
@@ -4,9 +4,14 @@
 """ Called by portal_tree for additional information on folder  """
 
 title_or_id = context.title_or_id()
-short_title = (len(title_or_id) > 15) and title_or_id[:15] + '...' or title_or_id
+l = len(title_or_id)
+ml = 25
+if l > ml:
+    short_title = title_or_id[:ml-6]+ '...' + title_or_id[l-3:]
+else:
+    short_title = title_or_id
 
 return {'title': context.Title(),
         'title_or_id': title_or_id,
-        'short_title': short_title,
+        'short_title': short_title.replace(' ', '&nbsp;'),
         }

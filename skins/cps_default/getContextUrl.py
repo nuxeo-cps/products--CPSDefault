9d7deee41ad9eb55db24108d3399ac2ea2fa933d
bdelbosc <devnull@localhost>
2003-06-05 10:03:09 +0000

    fixing getPortalUrl

--- skins/cps_default/getContextUrl.py
+++ skins/cps_default/getContextUrl.py
@@ -1,7 +1,10 @@
 ## Script (Python) "getPortalUrl"
 ##parameters=
 # $Id$
-rurl = context.portal_url(relative=1)
-rurl = rurl and '/' + rurl or ''
-
-return rurl
+utool = context.portal_url
+rurl = utool(relative=1)    # browser relative url
+cps_folder = context.getPhysicalPath()[1]
+cps_url = '/'
+if rurl.startswith(cps_folder):
+    cps_url += cps_folder + '/'
+return cps_url + utool.getRelativeUrl(context)

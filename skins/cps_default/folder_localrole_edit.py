c953e4a2f9c047419f35c409387574d64bf66c21
afernandez <devnull@localhost>
2003-06-12 09:15:02 +0000

    utilisation du PortalMembership

--- skins/cps_default/folder_localrole_edit.py
+++ skins/cps_default/folder_localrole_edit.py
@@ -5,23 +5,22 @@
 pm = context.portal_membership
 ids = context.REQUEST.get('member_ids', ())
 
-groupids = [group for group in ids if group.startswith('role:') ]
+group_ids = [group for group in ids if group.startswith('role:') ]
 member_ids = [ user for user in ids if not user.startswith('role:') ]
-
+member_role = context.REQUEST.get('member_role', '')
 
 if change_type == 'add':
     pm.setLocalRoles( obj=context
                     , member_ids=member_ids
-                    , member_role=context.REQUEST.get('member_role', '')
+                    , member_role=member_role
                     )
-    for groupid in groupids:
-        context.manage_addLocalGroupRoles(groupid=groupid, roles=[context.REQUEST.get('member_role'),])
-        
+    pm.setLocalGroupRoles( context, group_ids, member_role)
+    
 else:
     pm.deleteLocalRoles( obj=context
                        , member_ids=member_ids
                        )
-    context.manage_delLocalGroupRoles(groupids=groupids)
+    pm.deleteLocalGroupRoles(context, group_ids)
     
 qst='?portal_status_message=Local+Roles+changed.'
 

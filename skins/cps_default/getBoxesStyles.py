724b7bd36e825dde97dca8964086f39da54cf6c8
bdelbosc <devnull@localhost>
2003-07-24 08:22:09 +0000

    adding new format for base boxes

--- skins/cps_default/getBoxesStyles.py
+++ skins/cps_default/getBoxesStyles.py
@@ -2,18 +2,36 @@
 ##parameters=type=None
 # $Id$
 """Return the available boxes style and format."""
-# XXX TODO i18n / l10n 
+# XXX TODO i18n / l10n
 
 types = [
     {'type': 'basebox',
      'desc': 'Generic box',
      'fmt': [{'style': 'nuxeo',
-                  'format': 'default',
-                  'desc': 'default box'},
-                 ]
+              'format': 'default',
+              'desc': 'an empty default box'},
+             {'style': 'nuxeo',
+              'format': 'logo',
+              'desc': 'a top rigth nuxeo logo'},
+             {'style': 'nuxeo',
+              'format': 'search',
+              'desc': 'a top left search box'},
+             {'style': 'nuxeo',
+              'format': 'menu',
+              'desc': 'a top menu displaying 2 levels of sections and root of workspaces'},
+             {'style': 'nuxeo',
+              'format': 'breadcrumbs',
+              'desc': 'a breadcrumbs bar'},
+             {'style': 'nuxeo',
+              'format': 'l10n_select',
+              'desc': 'a locale selector'},
+             {'style': 'nuxeo',
+              'format': 'footer',
+              'desc': 'a nuxeo footer'},
+             ]
      },
     {'type': 'textbox',
-     'desc': 'Display static text', 
+     'desc': 'Display static text',
      'fmt': [{'style': 'nuxeo',
                   'format': 'default',
                   'desc': 'default box'},
@@ -22,12 +40,12 @@ types = [
     {'type': 'treebox',
      'desc': 'Display list of folders in a tree-like format',
      'fmt': [{'style': 'nuxeo',
-                  'format': 'default',
-                  'desc': 'box format'},
-                 {'style': 'nuxeo',
-                  'format': 'center',
-                  'desc': 'long format'},
-                 ]
+              'format': 'default',
+              'desc': 'box format'},
+             {'style': 'nuxeo',
+              'format': 'center',
+              'desc': 'long format'},
+             ]
      },
     {'type': 'contentbox',
      'desc': 'Display list of documents',
@@ -49,9 +67,9 @@ types = [
                   'desc': 'user box information'},
                  ]
      },
-    
+
     ]
- 
+
 if not type:
     return types
 
@@ -59,4 +77,4 @@ for t in types:
     if t['type'] == type:
         return t
 
-return 
+return

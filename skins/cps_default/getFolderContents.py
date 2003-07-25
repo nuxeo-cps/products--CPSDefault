be02db857b6acab0ad1771f9d9e393b9b713aa10
janguenot <devnull@localhost>
2003-07-25 16:56:50 +0000

    Getting the sorting preferences from a session object

--- skins/cps_default/getFolderContents.py
+++ skins/cps_default/getFolderContents.py
@@ -4,6 +4,11 @@
 """
 Get a sorted list of contents object
 """
+
+cps_cookie = context.REQUEST.SESSION.get('cps_preferences', {}) 
+sort_by    = cps_cookie.get('cps_display_order', 'titre');
+direction  = cps_cookie.get('cps_display_direction', 'asc');
+
 return context.filterContents(items=context.objectValues(),
                               sort_by=sort_by, direction=direction,
                               hide_folder=hide_folder)

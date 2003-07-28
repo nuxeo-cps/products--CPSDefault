64f8b29830b9f67e6802bfeff6de5832894d9163
bdelbosc <devnull@localhost>
2003-07-28 08:19:27 +0000

    order for param is 1/ args 2/ cookie 3/ default

--- skins/cps_default/getFolderContents.py
+++ skins/cps_default/getFolderContents.py
@@ -1,13 +1,15 @@
 ## Script (Python) "getFolderContents"
-##parameters=sort_by='title', direction='asc', hide_folder=0
+##parameters=sort_by=None, direction=None, hide_folder=0
 # $Id$
 """
 Get a sorted list of contents object
 """
-
-cps_cookie = context.REQUEST.SESSION.get('cps_preferences', {}) 
-sort_by    = cps_cookie.get('cps_display_order', 'titre');
-direction  = cps_cookie.get('cps_display_direction', 'asc');
+if not sort_by:
+    cps_cookie = context.REQUEST.SESSION.get('cps_preferences', {})
+    sort_by    = cps_cookie.get('cps_display_order', 'title');
+    direction  = cps_cookie.get('cps_display_direction', 'asc');
+elif not direction:
+    direction = 'asc'
 
 return context.filterContents(items=context.objectValues(),
                               sort_by=sort_by, direction=direction,

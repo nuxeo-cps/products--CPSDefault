70455178138a34f62afadcbda6791c0551117e1d
janguenot <devnull@localhost>
2003-07-30 09:27:20 +0000

    Adding moveTop/moveBottom features

---
+++ skins/cps_default/moveItemsBottom.py
@@ -0,0 +1,15 @@
+## Script (Python) "moveItemsBottom.py"
+##parameters=ids=[]
+# $Id$
+""" Move the selected objects up within the directory """
+
+context_url = context.REQUEST.get("context_url", context.getContextUrl())
+
+if not same_type(ids, []):
+    ids = [ids]
+
+for id in ids:
+    context.move_object_to_bottom(id)
+
+redirection_url = context_url + "/folder_contents"
+context.REQUEST.RESPONSE.redirect(redirection_url)
---
+++ skins/cps_default/moveItemsTop.py
@@ -0,0 +1,21 @@
+## Script (Python) "moveItemsTop"
+##parameters=ids=[]
+# $Id$
+""" Move the selected objects on the top within the directory """
+
+def cmp_desc(x, y):
+    return -cmp(x, y)
+
+context_url = context.REQUEST.get("context_url", context.getContextUrl())
+
+if not same_type(ids, []):
+    ids = [ids]
+
+# To respect the relative position of the choosen objects
+ids.sort(cmp_desc)
+
+for id in ids:
+    context.move_object_to_top(id)
+
+redirection_url = context_url + "/folder_contents"
+context.REQUEST.RESPONSE.redirect(redirection_url)

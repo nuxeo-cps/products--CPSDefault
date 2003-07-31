528cf104723f884b6ff8e671b4ab02624657f891
bdelbosc <devnull@localhost>
2003-07-31 10:02:52 +0000

    adding a center slot to skin folders

--- skins/cps_default/getBoxSlots.py
+++ skins/cps_default/getBoxSlots.py
@@ -3,4 +3,4 @@
 # $Id$
 """Return available slot for a box """
 
-return ('top', 'left', 'center_top', 'center_bottom', 'right', 'bottom')
+return ('top', 'left', 'center_top', 'center', 'center_bottom', 'right', 'bottom')

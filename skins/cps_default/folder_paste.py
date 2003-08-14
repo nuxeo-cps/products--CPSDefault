542b6a8e63421932d48334780a28be25a130692a
janguenot <devnull@localhost>
2003-08-14 11:23:16 +0000

    paste facility

---
+++ skins/cps_default/folder_paste.py
@@ -0,0 +1,17 @@
+##parameters=
+#$Id$
+"""
+Cut an object. Used within the folder_contents template.
+"""
+
+REQUEST=context.REQUEST
+
+if context.cb_dataValid:
+    context.manage_pasteObjects(REQUEST['__cp'])
+    message = 'psm_item(s)_pasted'
+else:
+    message = 'psm_copy_or_cut_at_least_one_document'
+
+return REQUEST.RESPONSE.redirect(context.absolute_url() + \
+                                 '/folder_contents?portal_status_message=%s' \
+                                 % (message, ))

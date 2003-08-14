8f95c005a31bd7c6c74380dda6f94857634a14af
janguenot <devnull@localhost>
2003-08-14 11:23:04 +0000

    copy facility

---
+++ skins/cps_default/folder_copy.py
@@ -0,0 +1,18 @@
+##parameters=
+#$Id$
+
+"""
+Copy an object. Used within the folder_contents template.
+"""
+
+REQUEST=context.REQUEST
+
+if REQUEST.has_key('ids'):
+    context.manage_copyObjects(REQUEST['ids'], REQUEST, REQUEST.RESPONSE)
+    message = 'psm_object_copied'
+else:
+    message = 'psm_select_at_least_one_document'
+
+return REQUEST.RESPONSE.redirect(context.absolute_url() + \
+                                 ('/folder_contents?portal_status_message=%s' \
+                                  % (message, )))

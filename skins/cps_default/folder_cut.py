de3de95dd81601b4a363d6f444973929ef27d288
janguenot <devnull@localhost>
2003-08-14 11:22:56 +0000

    cut facility

---
+++ skins/cps_default/folder_cut.py
@@ -0,0 +1,18 @@
+##parameters=
+#$Id$
+
+"""
+Cut an object. Used within the folder_contents template.
+"""
+
+REQUEST=context.REQUEST
+
+if REQUEST.has_key('ids'):
+    context.manage_cutObjects(REQUEST['ids'], REQUEST)
+    message = 'psm_item(s)_cut'
+else:
+    message = 'psm_select_at_least_one_document'
+
+return REQUEST.RESPONSE.redirect(context.absolute_url() + \
+                                 '/folder_contents?portal_status_message=%s' \
+                                 % (message, ))

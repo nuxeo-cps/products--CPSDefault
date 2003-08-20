364e1b9763a02905f65a3506007988527ac42301
janguenot <devnull@localhost>
2003-08-20 10:06:40 +0000

    Block the paste within sections for the moment

--- skins/cps_default/folder_paste.py
+++ skins/cps_default/folder_paste.py
@@ -6,12 +6,14 @@ Cut an object. Used within the folder_contents template.
 
 REQUEST=context.REQUEST
 
-if context.cb_dataValid:
-    context.manage_pasteObjects(REQUEST['__cp'])
-    message = 'psm_item(s)_pasted'
+if context.portal_type != 'Section':
+    if context.cb_dataValid:
+        context.manage_pasteObjects(REQUEST['__cp'])
+        message = 'psm_item(s)_pasted'
+    else:
+        message = 'psm_copy_or_cut_at_least_one_document'
 else:
-    message = 'psm_copy_or_cut_at_least_one_document'
-
+    message = 'psm_operation_not_allowed'
 return REQUEST.RESPONSE.redirect(context.absolute_url() + \
                                  '/folder_contents?portal_status_message=%s' \
                                  % (message, ))

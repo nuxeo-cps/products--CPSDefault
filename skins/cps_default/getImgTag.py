51f8bebc9911d319c8b935cdb13319dbeac39df7
bdelbosc <devnull@localhost>
2003-08-22 07:46:29 +0000

    catch exception on missing images

--- skins/cps_default/getImgTag.py
+++ skins/cps_default/getImgTag.py
@@ -5,7 +5,11 @@
 if not base_url:
     base_url = context.getBaseUrl()
 img_url = base_url + img_name
-img = context.restrictedTraverse(img_name)
+try:
+    img = context.restrictedTraverse(img_name)
+except KeyError:
+    return '<img src="' + img_url + '">'
+
 tag = '<img src="%s" width="%s" height="%s" border="0" ' % (
     img_url, img.width, img.height)
 if not title:

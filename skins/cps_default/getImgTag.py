c9a1d36cfda224ffc6b98dc98d59e2e80aaa1ee9
bdelbosc <devnull@localhost>
2003-08-27 13:33:39 +0000

    adding zomm height and width param

--- skins/cps_default/getImgTag.py
+++ skins/cps_default/getImgTag.py
@@ -1,19 +1,26 @@
 ## Script (Python) "getImgTag"
-##parameters=img_name, title=None, base_url=None
+##parameters=img_name, title=None, base_url=None, zoom=1, height=0, width=0
 # $Id$
 """ return an html img tag """
 if not img_name:
     return ''
-if not base_url:
+if base_url is None:
     base_url = context.getBaseUrl()
+elif base_url == '':
+    # img_name is a full path
+    img_url = img_name
+    
 img_url = base_url + img_name
 try:
     img = context.restrictedTraverse(img_name)
 except KeyError:
     return '<img src="' + img_url + '">'
-
+if not height:
+    height = int(zoom * getattr(img, 'height', ''))
+if not width:
+    width = int(zoom * getattr(img,'width', ''))
 tag = '<img src="%s" width="%s" height="%s" border="0" ' % (
-    img_url, getattr(img,'width', ''), getattr(img, 'height', ''))
+    img_url, width, height )
 if not title:
     title = img.title
 if title:

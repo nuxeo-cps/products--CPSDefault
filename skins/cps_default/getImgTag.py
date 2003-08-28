35e4eea52c7939b728a6a326f9c53276dbc2d0d8
janguenot <devnull@localhost>
2003-08-28 08:06:31 +0000

    Bug correction oon int()

--- skins/cps_default/getImgTag.py
+++ skins/cps_default/getImgTag.py
@@ -2,6 +2,7 @@
 ##parameters=img_name, title=None, base_url=None, zoom=1, height=0, width=0
 # $Id$
 """ return an html img tag """
+
 if not img_name:
     return ''
 if base_url is None:
@@ -9,16 +10,16 @@ if base_url is None:
 elif base_url == '':
     # img_name is a full path
     img_url = img_name
-    
+
 img_url = base_url + img_name
 try:
     img = context.restrictedTraverse(img_name)
 except KeyError:
     return '<img src="' + img_url + '">'
 if not height:
-    height = int(zoom * getattr(img, 'height', ''))
+    height = int(zoom * getattr(img, 'height', 0))
 if not width:
-    width = int(zoom * getattr(img,'width', ''))
+    width = int(zoom * getattr(img,'width', 0))
 tag = '<img src="%s" width="%s" height="%s" border="0" ' % (
     img_url, width, height )
 if not title:

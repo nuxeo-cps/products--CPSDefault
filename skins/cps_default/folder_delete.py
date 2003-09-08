0432315ee888f3fee1d1d4e1bf2c75a477fda588
sfermigier <devnull@localhost>
2003-09-08 17:19:48 +0000

    Coding style :(

--- skins/cps_default/folder_delete.py
+++ skins/cps_default/folder_delete.py
@@ -1,15 +1,14 @@
 ## Script (Python) "folder_delete"
 ##title=Delete objects from a folder
 ##parameters=
-REQUEST=context.REQUEST
+
+REQUEST = context.REQUEST
 ret_url = context.absolute_url() + '/folder_contents'
 
-if REQUEST.has_key( 'ids' ):
-  context.manage_delObjects( REQUEST['ids'] )
-  qs = '?portal_status_message=psm_item(s)_deleted'
-  
+if REQUEST.has_key('ids'):
+    context.manage_delObjects(REQUEST['ids'])
+    qs = '?portal_status_message=psm_item(s)_deleted'
 else:
-  qs = '?portal_status_message=psm_select_at_least_one_document'  
+    qs = '?portal_status_message=psm_select_at_least_one_document'  
 
-
-return REQUEST.RESPONSE.redirect( ret_url + qs )
+return REQUEST.RESPONSE.redirect(ret_url + qs)

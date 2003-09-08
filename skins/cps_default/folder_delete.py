7e71d736abb65f4681ea7f1d6d89c62a0a812710
hcauwelier <devnull@localhost>
2003-09-08 16:38:38 +0000

    customisation of the delete method to use a proper msgid. this script will be changed later to use the workflow.

---
+++ skins/cps_default/folder_delete.py
@@ -0,0 +1,15 @@
+## Script (Python) "folder_delete"
+##title=Delete objects from a folder
+##parameters=
+REQUEST=context.REQUEST
+ret_url = context.absolute_url() + '/folder_contents'
+
+if REQUEST.has_key( 'ids' ):
+  context.manage_delObjects( REQUEST['ids'] )
+  qs = '?portal_status_message=psm_item(s)_deleted'
+  
+else:
+  qs = '?portal_status_message=psm_select_at_least_one_document'  
+
+
+return REQUEST.RESPONSE.redirect( ret_url + qs )

24a3b4a7aa82def0afba04630aa6f51fd2644d55
hcauwelier <devnull@localhost>
2003-09-15 10:30:02 +0000

    translating the rename message (fixes #161: untranslated portal status message : Item(s) Renamed.)

---
+++ skins/cps_default/folder_rename.py
@@ -0,0 +1,16 @@
+## Script (Python) "folder_rename"
+##title=Rename Object
+##parameters=
+
+"""
+Rename objects which ids are passed in the request. Used within the
+folder_contents template.
+"""
+
+REQUEST = context.REQUEST
+
+context.manage_renameObjects(REQUEST['ids'], REQUEST['new_ids'], REQUEST)
+
+return REQUEST.RESPONSE.redirect(context.absolute_url() + \
+                                ('/folder_contents?portal_status_message=%s' \
+                                % ('psm_item(s)_renamed',)))

5f3146be8e9e357e41f311aa14b5b0000d0de256
epietrig <devnull@localhost>
2003-09-29 13:09:59 +0000

    used to format calendar dates (start and end dates)

---
+++ skins/cps_default/getSEDate.py
@@ -0,0 +1,18 @@
+## Script (Python) "getSEDate"
+##parameters=event=None, which_date='start_date'
+# $Id$
+""" returns a formatted start/end date from a document
+    implementing the event interface
+"""
+
+dt = None
+if event:
+    if which_date=='start':
+        dt = event.getContent().start()
+    elif which_date=='end':
+        dt = event.getContent().end()
+        
+if dt:
+    return context.getDateStr(dt,'short')
+else:
+    return ''

87fcbc64f57ab094628f9232808add29ecc50ca6
bdelbosc <devnull@localhost>
2003-09-30 10:02:44 +0000

    catch TimeError exception

--- skins/cps_default/getDateStr.py
+++ skins/cps_default/getDateStr.py
@@ -3,18 +3,21 @@
 # $Id$
 """ return a string date using the current locale """
 
-Localizer = context.Localizer
-mcat = Localizer.default
-
 if not dt:
     return ''
+
+Localizer = context.Localizer
+mcat = Localizer.default
 
 if fmt not in ['short', 'medium', 'long']:
     fmt = 'date_long'
 else:
     fmt = 'date_'+fmt
 
-ret = dt.strftime(mcat(fmt))
+try:
+    ret = dt.strftime(mcat(fmt))
+except 'TimeError':
+    ret = 'Invalid'
 
 return ret
 

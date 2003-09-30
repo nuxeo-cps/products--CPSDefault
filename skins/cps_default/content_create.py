08d4d307010bde71476b5f3bd7b0b1fce88eae71
janguenot <devnull@localhost>
2003-09-30 13:40:04 +0000

    CMF compatiblity now. You can use CMF Products within your CPS

--- skins/cps_default/content_create.py
+++ skins/cps_default/content_create.py
@@ -22,10 +22,19 @@ ob = getattr(context, id)
 
 try:
     doc = ob.getEditableContent()
-    doc.edit(**kw)
 except AttributeError:
     # not a proxy
     doc = ob
+else:
+    try:
+        doc.edit(**kw)
+    except:
+        # CMF Compatibility.
+        # type_name not necessarly and edit method
+        # for CMF types is not aware about that.
+        del kw['type_name']
+        doc.edit(**kw)
+
 context.portal_eventservice.notifyEvent('modify_object', context, {})
 context.portal_eventservice.notifyEvent('modify_object', ob, {})
 
@@ -33,7 +42,7 @@ context.portal_eventservice.notifyEvent('modify_object', ob, {})
 if REQUEST is not None:
     psm = 'psm_content_created'
     action_path = doc.getTypeInfo().immediate_view # getActionById('metadata')
-    REQUEST.RESPONSE.redirect('%s/%s?portal_status_message=%s' % 
+    REQUEST.RESPONSE.redirect('%s/%s?portal_status_message=%s' %
                               (ob.absolute_url(), action_path,
                                psm))
 

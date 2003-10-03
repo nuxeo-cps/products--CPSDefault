d5e6bd94a3d5240dacbfa1e7c8f6a39728cba5b5
janguenot <devnull@localhost>
2003-10-03 10:58:21 +0000

    CPS paste facility working now

--- skins/cps_default/folder_paste.py
+++ skins/cps_default/folder_paste.py
@@ -6,13 +6,18 @@ Cut an object. Used within the folder_contents template.
 
 REQUEST=context.REQUEST
 
-if context.portal_type != 'Section':
+#
+# Security to let the CPS up in error situation.
+# XXX : to remove if we you are sure.
+# Didn't see any case since now.
+#
+try:
     if context.cb_dataValid:
-        context.manage_pasteObjects(REQUEST['__cp'])
+        context.manage_CPSpasteObjects(REQUEST['__cp'])
         message = 'psm_item(s)_pasted'
     else:
         message = 'psm_copy_or_cut_at_least_one_document'
-else:
+except:
     message = 'psm_operation_not_allowed'
 return REQUEST.RESPONSE.redirect(context.absolute_url() + \
                                  '/folder_contents?portal_status_message=%s' \

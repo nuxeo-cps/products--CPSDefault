e0b0e7a1a56b7d531b1e04d9e7385ad78e4075eb
epietrig <devnull@localhost>
2003-10-15 15:29:35 +0000

    added new information for each folder: manager(s), used by the restricted sitemap (and by others, potentially)

--- skins/cps_default/getFolderInfo.py
+++ skins/cps_default/getFolderInfo.py
@@ -3,6 +3,8 @@
 # $Id$
 """ Called by portal_tree for additional information on folder  """
 
+from zLOG import LOG, DEBUG
+
 title_or_id = context.title_or_id()
 l = len(title_or_id)
 ml = 25
@@ -18,8 +20,29 @@ except AttributeError:
     doc = context
 description = doc.Description() or ''
 
+#get all managers of this folder
+#XXX: do something cleaner than that for dececting whether
+#we are in a section or a workspace
+if context.absolute_url().find('sections') != -1:
+    manager_role = 'SectionManager'
+else:
+    manager_role = 'WorkspaceManager'
+
+roles = context.portal_membership.getMergedLocalRoles(context)
+
+dtool = context.portal_metadirectories.members
+
+managers = []
+
+for pair in roles.items():
+    if pair[0].startswith('user:') and manager_role in pair[1]:
+        manager = pair[0][5:]
+        if dtool.getEntry(manager):
+            managers.append(manager)
+
 return {'title': context.Title(),
         'title_or_id': title_or_id,
         'short_title': short_title.replace(' ', '&nbsp;'),
         'description': description,
+        'managers': managers,
         }

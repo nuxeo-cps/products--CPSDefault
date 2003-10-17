1603b54c7a20c32473f552307a3074828d39bc80
epietrig <devnull@localhost>
2003-10-17 09:47:13 +0000

    added more params for treebox_edit_form

--- skins/cps_default/box_edit.py
+++ skins/cps_default/box_edit.py
@@ -33,7 +33,8 @@ if disp:
 
 # handle checkbox
 for k in ('display_in_subfolder', 'display_only_in_subfolder',
-          'children_only', 'contextual', 'zoom'):
+          'children_only', 'contextual', 'zoom','display_managers',
+          'authorized_only','show_root', 'display_description'):
     kw[k] = test(kw.has_key(k), 1, 0)
 
 # handle empty selection in contentbox_edit_form

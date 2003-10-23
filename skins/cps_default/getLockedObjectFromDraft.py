b6107cd24ab2e96192b1decb55987156c63f7a10
fguillaume <devnull@localhost>
2003-10-23 14:49:16 +0000

    Make it possible to get the draft from the locked object.

--- skins/cps_default/getLockedObjectFromDraft.py
+++ skins/cps_default/getLockedObjectFromDraft.py
@@ -1,8 +1,9 @@
-##parameters=
+##parameters=reverse=0
 """
 Find the locked object for a draft.
+If reverse==1, find the draft from the locked object.
 
-Returns the locked object or None.
+Returns an object or None.
 """
 # $Id$
 
@@ -10,13 +11,19 @@ wftool = context.portal_workflow
 folder = context.aq_parent
 docid = context.getDocid()
 flr = context.getFromLanguageRevisions()
+if not reverse:
+    want_review_state = 'locked'
+else:
+    want_review_state = 'draft'
 for ob in folder.objectValues():
     try:
-        rs = wftool.getInfoFor(ob, 'review_state', None)
-        if (rs == 'locked' and
-            ob.getDocid() == docid and
-            ob.getLanguageRevisions() == flr):
-            return ob
+        if ob.getDocid() != docid:
+            continue
+        if wftool.getInfoFor(ob, 'review_state', None) != want_review_state:
+            continue
+        if ob.getLanguageRevisions() != flr:
+            continue
+        return ob
     except:
         from zLOG import LOG
         LOG('getLockedObjectFromDraft', 200,

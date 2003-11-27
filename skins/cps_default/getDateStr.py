65e291f3cf93f9450f7a609368f3dda067bf9dd8
bdelbosc <devnull@localhost>
2003-11-27 09:17:42 +0000

    fixing strftime %p with LC_ALL=fr_FR enabling datetime ending with AM/PM

--- skins/cps_default/getDateStr.py
+++ skins/cps_default/getDateStr.py
@@ -14,7 +14,15 @@ else:
     fmt = 'date_'+fmt
 
 try:
-    ret = dt.strftime(mcat(fmt))
+    dfmt = mcat(fmt)
+    ret = dt.strftime(dfmt)
+    # space hack to fix %p strftime bug when LC_ALL=fr_FR
+    if (dfmt.endswith('%p') and not ret.endswith('M')):
+        h = int(dt.strftime('%H'))
+        if h > 12:
+            ret += ' PM'
+        else:
+            ret += ' AM'
 except 'TimeError':
     ret = 'Invalid'
 

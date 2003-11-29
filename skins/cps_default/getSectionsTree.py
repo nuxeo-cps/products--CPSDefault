30950b007b3129144de14b6b8aed7e109cd217ad
sfermigier <devnull@localhost>
2003-11-29 23:19:50 +0000

    Make lines < 80 characters.

--- skins/cps_default/getSectionsTree.py
+++ skins/cps_default/getSectionsTree.py
@@ -15,15 +15,15 @@ allowed_container_types = context.getAllowedContainerTypes(type_name)
 if allowed_container_types:
     for section in sections:
         if section['portal_type'] in allowed_container_types:
-            transitions =  wftool.getInitialTransitions(section['rpath'], type_name,
-                                                        wftool.TRANSITION_INITIAL_PUBLISHING)
+            transitions = wftool.getInitialTransitions(section['rpath'], 
+                 type_name, wftool.TRANSITION_INITIAL_PUBLISHING)
             transitions = [t for t in transitions if t in allowed_transitions]
             section['publishing_transitions'] = transitions
             res.append(section)
-else :
+else:
     for section in sections:
-        transitions =  wftool.getInitialTransitions(section['rpath'], type_name,
-                                                    wftool.TRANSITION_INITIAL_PUBLISHING)
+        transitions =  wftool.getInitialTransitions(section['rpath'],
+            type_name, wftool.TRANSITION_INITIAL_PUBLISHING)
         transitions = [t for t in transitions if t in allowed_transitions]
         section['publishing_transitions'] = transitions
         res.append(section)

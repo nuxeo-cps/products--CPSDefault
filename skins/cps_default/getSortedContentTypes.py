bff8e175b6b51bb55614bb0e01663e753ff03b18
bdelbosc <devnull@localhost>
2003-12-02 11:01:31 +0000

    fixing unicode pb using encode iso8859-15 thx encolpe

--- skins/cps_default/getSortedContentTypes.py
+++ skins/cps_default/getSortedContentTypes.py
@@ -3,24 +3,27 @@
 """ Sorting for display PortalFolder.py: allowedContentTypes """
 items = context.allowedContentTypes()
 
+def l10n(s):
+    cpsmcat = context.Localizer.default
+    ret = cpsmcat(s)
+    if same_type(ret, u''):
+        return ret.encode('iso-8859-15', 'ignore')
+    else:
+        return ret
+
 def cmp_type(a, b):
+    # we like workspaces and section
     if a.getId() in ('Workspace', 'Section'):
         return -1
     if b.getId() in ('Workspace', 'Section'):
         return 1
-    # we like workspaces and section
-    cpsmcat = context.Localizer.default
-    aa = cpsmcat(a.Title())
-    bb = cpsmcat(b.Title())
-    try:
-        return cmp(aa, bb)
-    except:
-        # XXX FIX ME if Title contains non US ascii char we have UnicodeError
-        # using unicode(aa) doesn't work :(
-        return 1
+    aa = l10n(a.Title()).lower()
+    bb = l10n(b.Title()).lower()
+    return cmp(aa, bb)
 
 items.sort(cmp_type)
 allowed = context.portal_types[context.getPortalTypeName()].allowed_content_types
 items = [i for i in items if i.getId() in allowed]
+items.sort(cmp_type)
 
 return items

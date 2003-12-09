1b359ca2a36b9296e3e750aa8a9ee055e25c2e54
bdelbosc <devnull@localhost>
2003-12-09 11:05:34 +0000

    addin cpsdefault info

--- skins/cps_devel/whereami.py
+++ skins/cps_devel/whereami.py
@@ -5,6 +5,7 @@
 utool = context.portal_url
 
 ob = context
+print 'Zope info ---------------'
 print 'object id (context):', ob.getId()
 print 'portal_type:', ob.getPortalTypeName()
 print ''
@@ -19,5 +20,12 @@ print 'getRelativeUrl:', utool.getRelativeUrl(ob)
 print 'aq_parent:', ob.aq_parent.id
 print 'aq_inner:', ob.aq_inner.id
 print 'container ob.aq_inner.aq_parent:', ob.aq_inner.aq_parent.id
+
+print
+print 'CPSDefault info ----------'
+print 'here_url = context.absolute_url: ', context.absolute_url()
+print 'base_url = context.getBaseUrl: ', context.getBaseUrl()
+print 'context_url = context.getContextUrl: ', context.getContextUrl()
+print 'in_ws = context.isInWorkspace: ', context.isInWorkspace()
 
 return printed 

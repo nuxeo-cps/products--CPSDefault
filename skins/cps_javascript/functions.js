ba520d702cbbffebb0ee304cfb691aa0c5c96890
madarche <devnull@localhost>
2004-01-05 09:26:33 +0000

    Putting JavaScript functions into a common file to increase template legibility

---
+++ skins/cps_javascript/functions.js
@@ -0,0 +1,42 @@
+
+function toggleFormTooltip(id) {
+    element = document.getElementById(id);
+    if (element) {
+	if (element.style.visibility == 'hidden') {
+	    element.style.visibility = 'visible';
+	} else {
+	    element.style.visibility = 'hidden';
+	}
+    }
+}
+
+function showFormTooltip(show, id) {
+    element = document.getElementById(id);
+    if (element) {
+	if (show) {
+	    element.style.visibility = 'visible';
+	} else {
+	    element.style.visibility = 'hidden';
+	}
+    }
+}
+
+isSelected = false;
+
+function toggleSelect(toggleSelectButton) {
+    formElements = toggleSelectButton.form.elements;
+
+    if (isSelected == false) {
+	for (i = 0; i < formElements.length; i++) {
+	    formElements[i].checked = true ;
+	}
+	isSelected = true;
+	//toggleSelectButton.value = "button_deselect_all";
+    } else {
+	for (i = 0; i < formElements.length; i++) {
+	    formElements[i].checked = false ;
+	}
+	isSelected = false;
+	//toggleSelectButton.value = "button_select_all";
+    }
+}

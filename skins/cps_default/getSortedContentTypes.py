0a620dd7180e8076387cdc596a05dcb4fdb73b3c
hcauwelier <devnull@localhost>
2004-01-06 14:41:26 +0000

    use new 'getAllowedPortalTypes' method from the workflow tool

--- skins/cps_default/getSortedContentTypes.py
+++ skins/cps_default/getSortedContentTypes.py
@@ -4,7 +4,7 @@
     if allowed=0 return Searchable portaltype"""
 
 if allowed:
-    items = context.allowedContentTypes()
+    items = context.portal_workflow.getAllowedPortalTypes(context)
 else:
     items = context.getSearchablePortalTypes()
 
@@ -25,13 +25,6 @@ def cmp_type(a, b):
     aa = l10n(a.Title()).lower()
     bb = l10n(b.Title()).lower()
     return cmp(aa, bb)
-
-if allowed:
-    ti = getattr(context.portal_types, context.getPortalTypeName(), None)
-    if ti is not None:
-        items = [i for i in items if i.getId() in ti.allowed_content_types]
-    else:
-        items = []
 
 items.sort(cmp_type)
 

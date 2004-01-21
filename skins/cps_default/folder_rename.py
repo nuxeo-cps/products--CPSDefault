3334eab5823aa48f5ef8f0c2833c2849cb438510
epietrig <devnull@localhost>
2004-01-21 14:18:30 +0000

    use computeId script to fix badly formed input IDs

--- skins/cps_default/folder_rename.py
+++ skins/cps_default/folder_rename.py
@@ -8,7 +8,9 @@ folder_contents template.
 
 REQUEST = context.REQUEST
 
-context.manage_renameObjects(REQUEST['ids'], REQUEST['new_ids'], REQUEST)
+new_ids = [context.computeId(id) for id in REQUEST['new_ids']]
+
+context.manage_renameObjects(REQUEST['ids'], new_ids, REQUEST) 
 
 return REQUEST.RESPONSE.redirect(context.absolute_url() + \
                                 ('/folder_contents?portal_status_message=%s' \

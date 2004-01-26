c5f627b2a3805f85e3110759105d4f546b2b159f
sfermigier <devnull@localhost>
2004-01-26 18:21:38 +0000

    Better looking, and more testable.

--- skins/cps_default/folder_localrole_edit.py
+++ skins/cps_default/folder_localrole_edit.py
@@ -1,30 +1,27 @@
-##parameters=change_type
+##parameters=change_type, member_ids=[], member_role='', REQUEST=None
 # $Id$
+
 pmtool = context.portal_membership
-ids = context.REQUEST.get('member_ids', ())
+ids = member_ids
 
-
-group_ids = [group[len('group:'):] for group in ids if group.startswith('group:') ]
-member_ids = [user[len('user:'):] for user in ids if user.startswith('user:') ]
-
-member_role = context.REQUEST.get('member_role', '')
+group_ids = [group[len('group:'):] 
+             for group in ids if group.startswith('group:') ]
+member_ids = [user[len('user:'):] 
+              for user in ids if user.startswith('user:') ]
 
 if change_type == 'add':
-    pmtool.setLocalRoles( obj=context
-                    , member_ids=member_ids
-                    , member_role=member_role
-                    )
-    pmtool.setLocalGroupRoles( context, group_ids, member_role)
+    pmtool.setLocalRoles(obj=context, member_ids=member_ids, 
+        member_role=member_role)
+    pmtool.setLocalGroupRoles(context, group_ids, member_role)
 
 else:
-    pmtool.deleteLocalRoles( obj=context
-                       , member_ids=member_ids
-                       )
+    pmtool.deleteLocalRoles( obj=context, member_ids=member_ids)
     pmtool.deleteLocalGroupRoles(context, group_ids, member_role)
 
-psm='psm_local_roles_changed'
+psm = 'psm_local_roles_changed'
 
-context.REQUEST.RESPONSE.redirect(
-    '%s/folder_localrole_form?portal_status_message=%s' %
-    (context.absolute_url(), psm))
+if REQUEST is not None:
+    REQUEST.RESPONSE.redirect(
+        '%s/folder_localrole_form?portal_status_message=%s' %
+        (context.absolute_url(), psm))
 

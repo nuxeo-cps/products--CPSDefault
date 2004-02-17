60d0234d724b8481482132332b769b99d571c556
hcauwelier <devnull@localhost>
2004-02-17 17:12:14 +0000

    Don't rely on the presence of REQUEST.

--- skins/cps_default/folder_copy.py
+++ skins/cps_default/folder_copy.py
@@ -11,6 +11,7 @@ if ids:
 else:
     message = 'psm_select_at_least_one_document'
 
-ret_url = context.absolute_url() + '/folder_contents'
-return REQUEST.RESPONSE.redirect(
-    ret_url + '?portal_status_message=%s' % message)
+if REQUEST is not None:
+    ret_url = context.absolute_url() + '/folder_contents'
+    return REQUEST.RESPONSE.redirect('%s?portal_status_message=%s' %
+                                        (ret_url, message))

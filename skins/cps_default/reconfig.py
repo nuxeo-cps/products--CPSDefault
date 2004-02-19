f5d7ccb6e478fdf9c189b2bc41770f446d12d70b
hcauwelier <devnull@localhost>
2004-02-19 09:18:11 +0000

    I quit, still use portal_properties and then treat enable_portal_joining.

--- skins/cps_default/reconfig.py
+++ skins/cps_default/reconfig.py
@@ -5,7 +5,9 @@
 template.
 """
 
-container.manage_editProperties(REQUEST)
-container.MailHost.smtp_host = REQUEST['smtp_server']
+container.portal_properties.editProperties(REQUEST)
+# manually update enable_portal_joining afterwards
+enable_portal_joining = getattr(REQUEST, 'enable_portal_joining', 0)
+container.manage_changeProperties(enable_portal_joining=enable_portal_joining)
 url = '%s/reconfig_form?portal_status_message=psm_portal_reconfigured'
 return REQUEST.RESPONSE.redirect(url % container.portal_url())

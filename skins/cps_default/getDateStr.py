acbf631ab52645d8e239402e66abdc0a6016a92c
bdelbosc <devnull@localhost>
2004-03-15 16:12:57 +0000

    adding iso8601 date format

--- skins/cps_default/getDateStr.py
+++ skins/cps_default/getDateStr.py
@@ -8,13 +8,16 @@ if not dt:
 Localizer = context.Localizer
 mcat = Localizer.default
 
-if fmt not in ('short', 'medium', 'long'):
+if fmt not in ('short', 'medium', 'long', 'iso8601'):
     fmt = 'date_long'
 else:
     fmt = 'date_' + fmt
 
 try:
-    dfmt = mcat(fmt)
+    if fmt == 'date_iso8601':
+        dfmt = '%Y-%m-%dT%H:%M:%SZ'
+    else:
+        dfmt = mcat(fmt)
     ret = dt.strftime(dfmt)
     # XXX remove this as soon as strftime is fixed
     # space hack to fix %p strftime bug when LC_ALL=fr_FR

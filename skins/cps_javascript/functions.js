b784b0c884c608219258b623f7a46b973e4e1f80
bdelbosc <devnull@localhost>
2004-03-22 15:39:39 +0000

    adding checkEmptySearch and setFocus + reindent

--- skins/cps_javascript/functions.js
+++ skins/cps_javascript/functions.js
@@ -8,17 +8,17 @@ function toggleSelect(toggleSelectButton, selectAllText, deselectAllText) {
     formElements = toggleSelectButton.form.elements;
 
     if (isSelected) {
-	for (i = 0; i < formElements.length; i++) {
-	    formElements[i].checked = false ;
-	}
-	isSelected = false;
-	toggleSelectButton.value = selectAllText;
+        for (i = 0; i < formElements.length; i++) {
+            formElements[i].checked = false ;
+        }
+        isSelected = false;
+        toggleSelectButton.value = selectAllText;
     } else {
-	for (i = 0; i < formElements.length; i++) {
-	    formElements[i].checked = true ;
-	}
-	isSelected = true;
-	toggleSelectButton.value = deselectAllText;
+        for (i = 0; i < formElements.length; i++) {
+            formElements[i].checked = true ;
+        }
+        isSelected = true;
+        toggleSelectButton.value = deselectAllText;
     }
 }
 
@@ -29,21 +29,53 @@ function toggleSelect(toggleSelectButton, selectAllText, deselectAllText) {
 function toggleFormTooltip(id) {
     element = document.getElementById(id);
     if (element) {
-	if (element.style.visibility == 'hidden') {
-	    element.style.visibility = 'visible';
-	} else {
-	    element.style.visibility = 'hidden';
-	}
+        if (element.style.visibility == 'hidden') {
+            element.style.visibility = 'visible';
+        } else {
+            element.style.visibility = 'hidden';
+        }
     }
 }
 
 function showFormTooltip(show, id) {
     element = document.getElementById(id);
     if (element) {
-	if (show) {
-	    element.style.visibility = 'visible';
-	} else {
-	    element.style.visibility = 'hidden';
-	}
+        if (show) {
+            element.style.visibility = 'visible';
+        } else {
+            element.style.visibility = 'hidden';
+        }
     }
+}
+
+
+//**********************************************************************
+function trim(s) {
+    while (s.substring(0,1) == ' ') {
+        s = s.substring(1,s.length);
+    }
+    while (s.substring(s.length-1,s.length) == ' ') {
+        s = s.substring(0,s.length-1);
+    }
+    return s;
+}
+
+
+//**********************************************************************
+function checkEmptySearch(formElem) {
+    var query = trim(formElem.SearchableText.value);
+    if (query != '') {
+        return true;
+    }
+    formElem.SearchableText.value = query
+    formElem.SearchableText.focus();
+    return false;
+}
+
+//**********************************************************************
+function setFocus() {
+    field = document.getElementById('field_focus')
+        if (field) {
+            field.focus();
+        }
 }

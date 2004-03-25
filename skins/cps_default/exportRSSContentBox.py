7d12d645d0d7d010a1496c804840622e70fbbd1e
bdelbosc <devnull@localhost>
2004-03-25 16:32:36 +0000

    fixing url for rss validator

--- skins/cps_default/exportRSSContentBox.py
+++ skins/cps_default/exportRSSContentBox.py
@@ -62,7 +62,7 @@ dc_keys = ('subject', 'date', 'creator',
 
 # computed value
 base_url = context.portal_url()+'/'
-channel_url = context.absolute_url() + '/exportRSS?' + \
+channel_url = context.absolute_url() + '/exportRSSContentBox?' + \
               context.REQUEST.environ.get('QUERY_STRING')
 channel_description = "RSS 1.0 export of the CPS Box named '%s' from the folder '%s'." % (
     box.title_or_id(), context.title_or_id())

e6280887261d1b3904e7f66e9d92cffe6a589c6d
bdelbosc <devnull@localhost>
2004-03-29 09:36:32 +0000

    untabify, fix indent and refactor trim

--- skins/cps_javascript/functions.js
+++ skins/cps_javascript/functions.js
@@ -1,78 +1,74 @@
-//**********************************************************************
-// Folder content
-//**********************************************************************
+//************************************************************
+// Nuxeo javascript functions
+// $Id$
 
+//************************************************************
+// Folder content
 isSelected = false;
 
 function toggleSelect(toggleSelectButton, selectAllText, deselectAllText) {
     formElements = toggleSelectButton.form.elements;
 
     if (isSelected) {
-	for (i = 0; i < formElements.length; i++) {
-	    formElements[i].checked = false;
-	}
-	isSelected = false;
-	toggleSelectButton.value = selectAllText;
+        for (i = 0; i < formElements.length; i++) {
+            formElements[i].checked = false;
+        }
+        isSelected = false;
+        toggleSelectButton.value = selectAllText;
     } else {
-	for (i = 0; i < formElements.length; i++) {
-	    formElements[i].checked = true;
-	}
-	isSelected = true;
-	toggleSelectButton.value = deselectAllText;
+        for (i = 0; i < formElements.length; i++) {
+            formElements[i].checked = true;
+        }
+        isSelected = true;
+        toggleSelectButton.value = deselectAllText;
     }
 }
 
-//**********************************************************************
+//************************************************************
 // Tooltips
-//**********************************************************************
-
 function toggleFormTooltip(id) {
     element = document.getElementById(id);
     if (element) {
-	if (element.style.visibility == 'hidden') {
-	    element.style.visibility = 'visible';
-	} else {
-	    element.style.visibility = 'hidden';
-	}
+        if (element.style.visibility == 'hidden') {
+            element.style.visibility = 'visible';
+        } else {
+            element.style.visibility = 'hidden';
+        }
     }
 }
 
 function showFormTooltip(show, id) {
     element = document.getElementById(id);
     if (element) {
-	if (show) {
-	    element.style.visibility = 'visible';
-	} else {
-	    element.style.visibility = 'hidden';
-	}
+        if (show) {
+            element.style.visibility = 'visible';
+        } else {
+            element.style.visibility = 'hidden';
+        }
     }
 }
 
-
-//**********************************************************************
+//************************************************************
 function trim(s) {
-    while (s.substring(0,1) == ' ') {
-	s = s.substring(1,s.length);
+    if(s) {
+        return s.replace(/^\s*|\s*$/g, "");
     }
-    while (s.substring(s.length-1,s.length) == ' ') {
-	s = s.substring(0,s.length-1);
-    }
-    return s;
+    return "";
 }
 
-
-//**********************************************************************
+//************************************************************
 function checkEmptySearch(formElem) {
     var query = trim(formElem.SearchableText.value);
     if (query != '') {
-	return true;
+        formElem.SearchableText.value = query;
+        return true;
     }
     formElem.SearchableText.value = query;
     formElem.SearchableText.focus();
     return false;
 }
 
-//**********************************************************************
+//************************************************************
 function setFocus() {
     field = document.getElementById('field_focus');
     if (field) {

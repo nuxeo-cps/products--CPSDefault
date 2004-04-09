775507c28619a8cc13b5373f05a9bc7904c93503
madarche <devnull@localhost>
2004-04-09 09:57:51 +0000

    Removing now useless debug

--- skins/cps_default/getSortedSubjects.py
+++ skins/cps_default/getSortedSubjects.py
@@ -4,12 +4,7 @@
     if allowed=0 return Searchable portaltype"""
 
 from Products.CMFCore.utils import getToolByName
-from zLOG import LOG, DEBUG
-
-logKey = 'getSortedSubjects'
 
 subjectVocabulary = getToolByName(context, 'portal_vocabularies').subject_voc
-LOG(logKey, DEBUG, "subjectVocabulary = %s" % str(subjectVocabulary))
-
 
 return subjectVocabulary

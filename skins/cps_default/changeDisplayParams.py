d58c0a89db1c475484543b4a4d0ec5c8037eb1dd
madarche <devnull@localhost>
2004-04-16 12:55:01 +0000

    Use the right quotes

--- skins/cps_default/changeDisplayParams.py
+++ skins/cps_default/changeDisplayParams.py
@@ -2,7 +2,7 @@
 # $Id$
 """ Change the order/style of the item display within a folder """
 
-context_url = REQUEST.get("context_url", context.getContextUrl())
+context_url = REQUEST.get('context_url', context.getContextUrl())
 
 # Retrieve display preference from session
 display_params = REQUEST.SESSION.get('cps_display_params', {})
@@ -11,9 +11,9 @@ form = REQUEST.form
 
 if form is not None:
     # Order and direction
-    display_order = form.get("display_order")
+    display_order = form.get('display_order')
     # "None" means use folder ordering
-    if display_order == "None" or not display_order:
+    if display_order == 'None' or not display_order:
         sort_by = None
         direction = None
     else:
@@ -23,7 +23,7 @@ if form is not None:
     display_params['direction'] = direction
 
     # Style
-    format = form.get("display_style")
+    format = form.get('display_style')
     if format:
         # XXX:
         # "None" means default here whereas None means "don't change"
@@ -34,5 +34,5 @@ if form is not None:
     # Update session
     REQUEST.SESSION['cps_display_params'] = display_params
 
-redirection_url = context_url + "/folder_contents"
+redirection_url = context_url + '/folder_contents'
 REQUEST.RESPONSE.redirect(redirection_url)

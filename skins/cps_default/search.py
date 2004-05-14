d85a7b80aea3ff194589badc349cbdd86aa89440
bdelbosc <devnull@localhost>
2004-05-14 16:11:22 +0000

    refactoring search using new indexes

--- skins/cps_default/search.py
+++ skins/cps_default/search.py
@@ -1,110 +1,53 @@
-##parameters=REQUEST=None, query={}, sort_by=None, direction=None, hide_folder=0, folder_prefix=None, start_date=None, end_date=None, allow_empty_search=0
+##parameters=REQUEST=None, query={}, sort_by=None, direction=None, hide_folder=1, folder_prefix=None, start_date=None, end_date=None, allow_empty_search=0, sortt_limit=100
 # $Id$
 """ return a list of proxy matching the query """
 
 from zLOG import LOG, DEBUG
 
+
 if REQUEST is not None:
     query.update(REQUEST.form)
 
-# check for empty search:
-if not allow_empty_search:
-    qv = filter(None, query.values())
-    if not qv or (len(qv) == 2 and
-                  str(query.get('modified')) == '1970/01/01'):
-        return []
+if str(query.get('modified')) == '1970/01/01':
+    del query['modified']
+    if query.has_key('modified_usage'):
+        del query['modified_usage']
 
+if not allow_empty_search and not query:
+    return []
+
+bmt = context.Benchmarktimer('search query %s' % query)
+
+if folder_prefix and not query.has_key('path'):
+    query['path'] =  context.getBaseUrl + folder_prefix
+
+# use the cps searchable set to remove objects in 'portal_*' or named '.foo'
+query['cps_filter_sets'] = 'searchable'
+
+if hide_folder:
+    query['cps_filter_sets'] = {'query' : ('searchable', 'leaves'),
+                               'operator' : 'and'}
+
+# XXX TODO make start/end search
+
+
+# sorting
+if sort_by and not query.has_key('sort-on'):
+    query['sort-on'] = sort_by
+    if direction and not query.has_key('sort-order'):
+        if direction.startswith('desc'):
+            query['sort-order'] = 'reverse'
+    if sort_limit and not query.has_key('sort-limit'):
+        query['sort-limit'] = sort_limit
+
+bmt.setMarker('start catalog search for %s')
 catalog = context.portal_catalog
-ptool = context.portal_proxies
+brains = catalog(**query)
+bmt.setMarker('stop catalog search')
 
-# get searchable portal type only
-okpt = context.getSearchablePortalTypes(only_ids=1)
-pt = query.get('portal_type', None)
-if pt is not None:
-    # The query can contain a single portal_type or a list of portal_types
-    if same_type(pt, ''):
-        pt = [pt]
+bmt.setMarker('stop')
+bmt.saveProfile(context.REQUEST)
+# no more need to use filterContents
 
-del_portal_type = 0
-if pt and pt != ['']:
-    pt = [t for t in pt if t in okpt]
-else:
-    del_portal_type = 1
-    pt = okpt
-query['portal_type'] = pt
+return brains
 
-# search the document repository
-portal_path = context.portal_url.getPortalPath()
-query['path'] = portal_path+'/portal_repository/'
-
-# init status
-status=''
-if query.get('review_state'):
-    status = query['review_state']
-    wtool = context.portal_workflow
-    del query['review_state']
-
-# search and get documents brains
-# XXX : When this script is called from the unit tests environment no document
-# brains are ever returned by the catalog. Why ?
-b_docs = catalog(**query)
-
-# restore query as it is pass trought request.form
-del query['path']
-if del_portal_type:
-    del query['portal_type']
-if status:
-    query['review_state'] = status
-
-
-items = []
-for b_doc in b_docs:
-    doc_id = b_doc.getPath().split('/')[-1]
-    # get all visible proxies information with the same doc_id
-    i_proxies = ptool.getProxiesFromObjectId(doc_id,
-                                             proxy_rpath_prefix=folder_prefix)
-    for i_proxy in i_proxies:
-        proxy = i_proxy['object']
-
-        # prevent zcatalog desynchronization ??
-        try:
-            title = proxy.Title()
-        except (AttributeError):
-            continue
-
-        # status filtering
-        if (status and
-            wtool.getInfoFor(proxy, 'review_state','nostate') != status):
-            continue
-
-        # event start/end filtering
-        if start_date and end_date:
-            doc = proxy.getContent()
-            sd = ed = 0
-            if hasattr(doc.aq_explicit, 'start'):
-                if callable(doc.start):
-                    sd = doc.start()
-                else:
-                    sd = doc.start
-            if hasattr(doc.aq_explicit, 'end'):
-                if callable(doc.end):
-                    ed = doc.end()
-                else:
-                    ed = doc.end
-            if not sd or not ed:
-                continue
-            if (sd - end_date > 0) or (start_date - ed > 0):
-                continue
-
-        items.append(proxy)
-
-if not sort_by:
-    disp_params = context.REQUEST.SESSION.get('cps_display_params', {})
-    sort_by    = disp_params.get('sort_by', 'title');
-    direction  = disp_params.get('direction', 'asc');
-elif not direction:
-    direction = 'asc'
-
-return context.filterContents(items=items,
-                              sort_by=sort_by, direction=direction,
-                              hide_folder=hide_folder)

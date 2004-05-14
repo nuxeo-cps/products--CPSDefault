35ada5a643ff93f556d4e24c31c6b4df39b85303
bdelbosc <devnull@localhost>
2004-05-14 16:12:44 +0000

    display proxy and doc indexes

--- skins/cps_devel/whereami.py
+++ skins/cps_devel/whereami.py
@@ -1,50 +1,59 @@
-## Script (Python) "whereami.py"
-##parameters=
+##parameters=proxy=None
 ##
 
 utool = context.portal_url
 
-ob = context
+if proxy is None:
+    proxy = context
+
 print '<pre>'
 print
 print 'Zope info ---------------'
-print 'object id (context):', ob.getId()
-print 'portal_type:', ob.getPortalTypeName()
+print 'object id (context):', proxy.getId()
+print 'portal_type:', proxy.getPortalTypeName()
 print ''
-print 'absolute_url:', ob.absolute_url()
-print 'absolute_url(relative=1):', ob.absolute_url(relative=1)
-print 'getPhysicalPath:', ob.getPhysicalPath()
-print 'getRelativeContentUrl:', utool.getRelativeContentURL(ob)
-print 'getRelativeContentPath:', utool.getRelativeContentPath(ob)
-print 'getRelativeUrl:', utool.getRelativeUrl(ob)
+print 'absolute_url:', proxy.absolute_url()
+print 'absolute_url(relative=1):', proxy.absolute_url(relative=1)
+print 'getPhysicalPath:', proxy.getPhysicalPath()
+print 'getRelativeContentUrl:', utool.getRelativeContentURL(proxy)
+print 'getRelativeContentPath:', utool.getRelativeContentPath(proxy)
+print 'getRelativeUrl:', utool.getRelativeUrl(proxy)
 
 
-print 'aq_parent:', ob.aq_parent.id
-print 'aq_inner:', ob.aq_inner.id
-print 'container ob.aq_inner.aq_parent:', ob.aq_inner.aq_parent.id
+print 'aq_parent:', proxy.aq_parent.id
+print 'aq_inner:', proxy.aq_inner.id
+print 'container proxy.aq_inner.aq_parent:', proxy.aq_inner.aq_parent.id
 
 print
 print 'CPSDefault info ----------'
-print 'here_url = context.absolute_url: ', context.absolute_url()
-print 'base_url = context.getBaseUrl: ', context.getBaseUrl()
-print 'context_url = context.getContextUrl: ', context.getContextUrl()
-print 'in_ws = context.isInWorkspace: ', context.isInWorkspace()
+print 'here_url = proxy.absolute_url: ', proxy.absolute_url()
+print 'base_url = proxy.getBaseUrl: ', proxy.getBaseUrl()
+print 'context_url = proxy.getContextUrl: ', proxy.getContextUrl()
+print 'in_ws = proxy.isInWorkspace: ', proxy.isInWorkspace()
 print
 
 try:
-    doc = context.getContent()
+    doc = proxy.getContent()
 except:
-    doc = context
+    doc = proxy
 
-print 'CPSDocument ------------'
-path = '/'.join(doc.getPhysicalPath())
-print 'doc path: %s' % path
+
+doc_path = '/'.join(doc.getPhysicalPath())
+proxy_path = '/'.join(proxy.getPhysicalPath())
+
+print 'ZCatalog proxy ---------------'
+proxy_rid = proxy.portal_catalog.getrid(proxy_path)
+print 'proxy %s' % proxy_path
+print 'zcat.getrid(%s) = %s' % (proxy_path, proxy_rid)
+print '<a href="portal_catalog/manage_objectInformation?rid=%s">view catalog info</a>' % proxy_rid
 print
 
-print 'ZCatalog ---------------'
-rid = doc.portal_catalog.getrid(path)
-print 'zcat.getrid(%s) = %s' % (path, rid)
-print '<a href="portal_catalog/manage_objectInformation?rid=%s>view catalog info</a>' % rid
+print 'ZCatalog doc --------------'
+doc_rid = proxy.portal_catalog.getrid(doc_path)
+print 'doc %s' % doc_path
+print 'zcat.getrid(%s) = %s' % (doc_path, doc_rid)
+print '<a href="portal_catalog/manage_objectInformation?rid=%s">view catalog info</a>' % doc_rid
+
 print
 
 try:

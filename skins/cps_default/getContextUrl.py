8e3f0172ccfe246d45b543e0a9559bb90ce8ab45
afernandez <devnull@localhost>
2004-05-25 12:25:11 +0000

    if object is a brain use the pointed object instead

--- skins/cps_default/getContextUrl.py
+++ skins/cps_default/getContextUrl.py
@@ -5,11 +5,16 @@
 if not utool:
     utool = context.portal_url
 base_url = context.getBaseUrl(utool=utool)
+
 if not obj:
     obj = context
+
+if obj.meta_type == 'CMF Catalog' and hasattr(obj, 'getObject'):
+    obj = obj.getObject()
 
 context_url = base_url + utool.getRelativeUrl(obj)
 if concat and context_url[-1] == '/':
     context_url = context_url[:-1]
+
 
 return context_url

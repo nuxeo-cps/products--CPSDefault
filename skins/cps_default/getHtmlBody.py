21d1955a8c61ac528ecef2c8d9cc038b532a3969
madarche <devnull@localhost>
2004-06-21 12:35:23 +0000

    Importing the regexp method directly from the ZPT and not using an additional script just for import

--- skins/cps_default/getHtmlBody.py
+++
@@ -1,9 +0,0 @@
-##parameters=html_content
-# $Id$
-"""Return the content of the <body> tag of an HTML document.
-"""
-
-from Products.CPSDefault.utils import getHtmlBody
-
-return getHtmlBody(html_content)
-
--- skins/cps_default/getNonArchivedVersionContextUrl.py
+++
@@ -1,8 +0,0 @@
-##parameters=content_url
-# $Id$
-"""Removes the reference to an archived version if there is one present.
-"""
-
-from Products.CPSDefault.utils import getNonArchivedVersionContextUrl
-
-return getNonArchivedVersionContextUrl(content_url)

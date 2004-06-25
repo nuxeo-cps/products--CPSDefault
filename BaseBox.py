2a4279908df774ec24f13bdcd573e77dbacafc69
atchertchian <devnull@localhost>
2004-06-25 16:52:55 +0000

    view permission on a base box is set to anonymous users when creating the box

--- BaseBox.py
+++ BaseBox.py
@@ -61,6 +61,7 @@ if 'cps_is_portalbox' not in ftiprops_ids:
 def addBaseBox(dispatcher, id, REQUEST=None):
     """Add a Base Box."""
     ob = BaseBox(id)
+    ob.setPermission(View, 1, ('Anonymous',))
     container = dispatcher.Destination()
     container._setObject(id, ob)
     if REQUEST is not None:

9fe7908fa2dec7f4891e62fbd29aa6d373603ba3
atchertchian <devnull@localhost>
2004-06-28 11:39:08 +0000

    removed misplaced and erroneous setPermission View on Anonymous users

--- BaseBox.py
+++ BaseBox.py
@@ -61,8 +61,6 @@ if 'cps_is_portalbox' not in ftiprops_ids:
 def addBaseBox(dispatcher, id, REQUEST=None):
     """Add a Base Box."""
     ob = BaseBox(id)
-    # XXX what for ? 
-    #ob.setPermission(View, 1, ('Anonymous',))
     container = dispatcher.Destination()
     container._setObject(id, ob)
     if REQUEST is not None:
@@ -159,6 +157,7 @@ class BaseBox(PortalContent, DefaultDublinCoreImpl, PropertyManager):
         self.display_in_subfolder = display_in_subfolder
         self.display_only_in_subfolder = display_only_in_subfolder
         self.locked = locked
+
 
     #
     # ZMI

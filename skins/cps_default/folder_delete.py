1c30915e41c955345908e7f98d1b8ac6bcad0d57
janguenot <devnull@localhost>
2004-07-19 14:41:47 +0000

    Send a fake event simulating workflow (need if for CPSSubscriptions) while waiting for the workflow to cope with that

--- skins/cps_default/folder_delete.py
+++ skins/cps_default/folder_delete.py
@@ -4,10 +4,17 @@
 ret_url = context.absolute_url() + '/folder_contents'
 
 if ids:
+    for id in ids:
+        # XXX has to be called from here since the workflow doesn't handle the
+        # deletion yet
+        ob = getattr(context, id)
+        context.portal_eventservice.notifyEvent('workflow_delete',
+                                                ob,
+                                                {})
     context.manage_delObjects(ids)
     message = 'portal_status_message=psm_item(s)_deleted'
 else:
-    message = 'portal_status_message=psm_select_at_least_one_document'  
+    message = 'portal_status_message=psm_select_at_least_one_document'
 
 if REQUEST is not None:
     return REQUEST.RESPONSE.redirect(ret_url + '?' + message)

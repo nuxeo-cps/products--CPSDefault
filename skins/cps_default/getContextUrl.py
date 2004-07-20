b7708b06edb683f22d12f3fe956c5ce7b2332a86
dwyart <devnull@localhost>
2004-07-20 09:07:07 +0000

    Use same check for brains as in getContentInfo

--- skins/cps_default/getContextUrl.py
+++ skins/cps_default/getContextUrl.py
@@ -9,12 +9,12 @@ base_url = context.getBaseUrl(utool=utool)
 if not obj:
     obj = context
 
-if obj.meta_type == 'CMF Catalog' and hasattr(obj, 'getObject'):
+if hasattr(obj.aq_explicit, 'getRID'):
+    # obj is a brain
     obj = obj.getObject()
 
 context_url = base_url + utool.getRelativeUrl(obj)
 if concat and context_url[-1] == '/':
     context_url = context_url[:-1]
-
 
 return context_url

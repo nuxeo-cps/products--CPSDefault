55ff7cf97b354e80725ebe720b58974b92fd2b07
hcauwelier <devnull@localhost>
2004-08-19 15:03:14 +0000

    Simplifying (and a bit optimising) the code.

--- skins/cps_default/getSectionsTree.py
+++ skins/cps_default/getSectionsTree.py
@@ -5,27 +5,23 @@
 sections = context.portal_trees.sections.getList()
 
 wftool = context.portal_workflow
+getInitialTransitions = wftool.getInitialTransitions
+TRANSITION_INITIAL_PUBLISHING = wftool.TRANSITION_INITIAL_PUBLISHING
 allowed_transitions = wftool.getAllowedPublishingTransitions(context)
+
 type_name = context.portal_type
+allowed_container_types = context.getAllowedContainerTypes(type_name)
 
 res = []
 
-allowed_container_types = context.getAllowedContainerTypes(type_name)
+for section in sections:
+    if allowed_container_types is not None and not (
+            section['portal_type'] in allowed_container_types):
+        continue
+    transitions = getInitialTransitions(section['rpath'], type_name,
+                                        TRANSITION_INITIAL_PUBLISHING)
+    transitions = [t for t in transitions if t in allowed_transitions]
+    section['publishing_transitions'] = transitions
+    res.append(section)
 
-if allowed_container_types:
-    for section in sections:
-        if section['portal_type'] in allowed_container_types:
-            transitions = wftool.getInitialTransitions(section['rpath'], 
-                 type_name, wftool.TRANSITION_INITIAL_PUBLISHING)
-            transitions = [t for t in transitions if t in allowed_transitions]
-            section['publishing_transitions'] = transitions
-            res.append(section)
-else:
-    for section in sections:
-        transitions =  wftool.getInitialTransitions(section['rpath'],
-            type_name, wftool.TRANSITION_INITIAL_PUBLISHING)
-        transitions = [t for t in transitions if t in allowed_transitions]
-        section['publishing_transitions'] = transitions
-        res.append(section)
-        
 return res

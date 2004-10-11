263f5d16c6487628a618a4f5ff1186bbe5a626fe
edegoute <devnull@localhost>
2004-10-11 12:30:46 +0000

    improved by J.-M. O.

--- skins/cps_default/getBaseUrl.py
+++ skins/cps_default/getBaseUrl.py
@@ -13,7 +13,14 @@ else:
 
 if path_info.startswith('/VirtualHostBase/') :
     # apache detection
-    return '/'
+    # Inside-out hosting (VHM _vh_)
+    if path_info.find('_vh_') > 0:
+        base = path_info.split('_vh_')[1]
+        if base.find('/') > 0:
+            base = base.split('/')[0]
+        return '/' + base + '/'
+    else:
+        return '/'
 else:
     # XXX squid detection
     # classic case

9dfdffc17d28f81445d64f92263351494c4561d3
madarche <devnull@localhost>
2004-10-22 14:20:45 +0000

    - Added more possibilities to getDateStr.py to display dates according to the   ISO 8601 format and W3C recommendations.
    (I am interested if someone finds better names than the following ones:
     'iso8601_medium_easy', 'iso8601_long_easy')

--- skins/cps_default/getDateStr.py
+++ skins/cps_default/getDateStr.py
@@ -8,14 +8,25 @@ if not dt:
 Localizer = context.Localizer
 mcat = Localizer.default
 
-if fmt not in ('short', 'medium', 'long', 'iso8601'):
-    fmt = 'date_long'
-else:
+if fmt in ('short', 'medium', 'long'):
+    # This string will be used to retrieve format in the right .po file
     fmt = 'date_' + fmt
+# For iso8601 dates read http://www.w3.org/TR/NOTE-datetime
+elif fmt not in ('iso8601', 'iso8601_short', 'iso8601_medium', 'iso8601_long',
+                 'iso8601_medium_easy', 'iso8601_long_easy'):
+    fmt = 'iso8601_medium_easy'
 
 try:
-    if fmt == 'date_iso8601':
+    if fmt == 'iso8601_short' or fmt == 'iso8601':
+        dfmt = '%Y-%m-%d'
+    elif fmt == 'iso8601_medium':
+        dfmt = '%Y-%m-%dT%H:%MZ'
+    elif fmt == 'iso8601_medium_easy':
+        dfmt = '%Y-%m-%d %H:%M'
+    elif fmt == 'iso8601_long':
         dfmt = '%Y-%m-%dT%H:%M:%SZ'
+    elif fmt == 'iso8601_long_easy':
+        dfmt = '%Y-%m-%d %H:%M:%S'
     else:
         dfmt = mcat(fmt)
     ret = dt.strftime(dfmt)

a75bf00f099ccc81591a11747e9f41b8117a13e6
madarche <devnull@localhost>
2004-12-11 20:58:31 +0000

    - Made computeId.py not cut words in the ids it generates.

--- skins/cps_default/computeId.py
+++ skins/cps_default/computeId.py
@@ -1,8 +1,9 @@
-##parameters=compute_from='', max_chars_for_id=20, location=None
+##parameters=compute_from='', max_chars_for_id=20, location=None, portal_type=None
 
 # $Id$
-"""
-Return a new id computed from compute_from
+"""Generate an id from a given string.
+
+This method avoids collisions.
 """
 
 from string import maketrans
@@ -12,56 +13,77 @@ from Products.CMFCore.utils import getToolByName
 from zLOG import LOG, DEBUG
 
 GENERATION_MAX_TRIES = 500
-def generateNewId(newid):
-    """Generate new id.
+def generateNewId(id):
+    """Generate annew id.
 
     This is to be used to avoid collisions.
     """
-    m = re.match('(.*)\d\d\d\d$', newid)
+    m = re.match('(.*)\d\d\d\d$', id)
     if m is not None:
         prefix = m.group(1)
     else:
-        prefix = newid
+        prefix = id
 
     tries = 0
     while 1:
         tries += 1
         if tries == GENERATION_MAX_TRIES:
             # grow prefix
-            prefix = newid
+            prefix = id
             tries = 0
         suffix = str(randrange(1000, 10000))
-        newid = prefix + suffix
-        return newid
+        id = prefix + suffix
+        return id
 
 
-# Create, no id, get from title
-newid = compute_from.strip()[:max_chars_for_id]
+# Initialization
+id = compute_from
+#LOG('computeId', DEBUG, 'init = %s' % id)
 
 # Normalization
-newid = newid.replace('∆', 'AE')
-newid = newid.replace('Ê', 'ae')
-newid = newid.replace('º', 'OE')
-newid = newid.replace('Ω', 'oe')
-newid = newid.replace('ﬂ', 'ss')
+id = id.replace('∆', 'AE')
+id = id.replace('Ê', 'ae')
+id = id.replace('º', 'OE')
+id = id.replace('Ω', 'oe')
+id = id.replace('ﬂ', 'ss')
 tr = maketrans(
     r"'\;/ &:¿¡¬√ƒ≈«»… ÀÃÕŒœ—“”‘’÷ÿŸ⁄€‹›‡·‚„‰ÂÁËÈÍÎÏÌÓÔÒÚÛÙıˆ¯˘˙˚¸˝ˇ",
     r'_______AAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy')
-newid = newid.translate(tr)
-ok = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_.'
-newid = ''.join([c for c in newid if c in ok])
-while newid.startswith('_') or newid.startswith('.'):
-    newid = newid[1:]
+id = id.translate(tr)
+ok_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_.'
+id = ''.join([c for c in id if c in ok_chars])
+id = id.lower()
+#LOG('computeId', DEBUG, 'normal = %s' % id)
 
-while newid.endswith('_'):
-    newid = newid[:-1]
+# Avoiding duplication of meaningless chars
+id = re.sub('-+', '-', id)
+id = re.sub('_+', '_', id)
+id = re.sub('\.+', '.', id)
+#LOG('computeId', DEBUG, 'duplicate = %s' % id)
 
-if newid:
-    newid = newid.lower()
-else:
-    # Fallback if empty
-    newid = str(int(DateTime())) + str(randrange(1000, 10000))
+# Avoiding annoying presence of meaningless chars
+while id.startswith('-') or id.startswith('_') or id.startswith('.'):
+    id = id[1:]
+while id.endswith('-') or id.endswith('_') or id.endswith('.'):
+    id = id[:-1]
+#LOG('computeId', DEBUG, 'meaningless = %s' % id)
 
+# Fallback if empty
+if not id:
+    #LOG('computeId', DEBUG, 'fallback = %s' % id)
+    id = str(int(DateTime())) + str(randrange(1000, 10000))
+
+# Preventing word cuts
+words = re.split('-*_*\.*\s*', id)
+#LOG('computeId', DEBUG, 'words = %s' % words)
+# TODO: remove from words all the meaningless words according to a
+# specified locale.
+id = words[0] # The id needs to contain at least one word
+words = words[1:]
+while words and ((len(id) + len(words[0]) + 1) <= max_chars_for_id):
+    id = "%s_%s" % (id, words[0])
+    words = words[1:]
+#LOG('computeId', DEBUG, 'wordcuts = %s' % id)
 
 # Get the container in which we want the new object to be created.
 if location is not None:
@@ -73,11 +95,11 @@ else:
 # acquisition is a pain) and that the id is not used in the given container.
 portal = getToolByName(context, 'portal_url').getPortalObject()
 # It's needed to allow index_html for join code
-while (hasattr(portal, newid)
-       and newid not in ('index_html', 'sections', 'workspaces')
-       or hasattr(container.aq_explicit, newid)):
+while (hasattr(portal, id)
+       and id not in ('index_html', 'sections', 'workspaces')
+       or hasattr(container.aq_explicit, id)):
     # The id is reserved we need to compute another id
-    newid = generateNewId(newid)
+    id = generateNewId(id)
 
-return newid
+return id
 

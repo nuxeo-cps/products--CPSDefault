5a4dbec6e4e8ee3cc02f36bbacdc7c8fd3b878c8
tziade <devnull@localhost>
2005-01-20 15:25:32 +0000

    moved + setFocus on search forms

---
+++ skins/cps_default/getOnLoad.py
@@ -0,0 +1,14 @@
+## Script (Python) "getOnLoad.py"
+#parameters=
+##$Id$
+""" return the body onload attribute content
+"""
+request = container.REQUEST
+
+if request.has_key('URL'):
+    URL = container.REQUEST['URL']
+    if URL is not None and \
+        (URL.endswith('search_form') or URL.endswith('advanced_search_form')):
+        return 'highlightSearchTerm();setFocus();'
+
+return 'setFocus();'

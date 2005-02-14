9328d17e9d26fc1cdcf78cb1ad6bdcd62da59428
edegoute <devnull@localhost>
2005-02-14 10:24:58 +0000

    move managing language util in CPSCore.utils

--- skins/cps_default/manageCPSLanguage.py
+++ skins/cps_default/manageCPSLanguage.py
@@ -1,52 +1,20 @@
-##parameters=REQUEST=None, **kw
+##parameters
 # -*- coding: iso-8859-15 -*-
 # $Id$
 """Make available a new language in a CPS portal with Localizer"""
+from Products.CPSCore.utils import manageCPSLanguage
 
-if REQUEST is not None:
-    kw.update(REQUEST.form)
-
-action = kw.get('action')
-languages = kw.get('languages')
-lang = kw.get('language')
-catalogs = context.Localizer.objectValues()
-catalogs.append(context.Localizer)
-portal = context.portal_url.getPortalObject()
-context_url = portal.absolute_url()
+request = context.REQUEST
+context_url = context.portal_url.getPortalObject().absolute_url()
 template = 'language_manage_form'
+psm = ''
 
-if same_type(languages, ''):
-    languages = [languages]
+if request is not None:
+    kw = {}
+    kw.update(request.form)
+    psm = manageCPSLanguage(context, **kw)
 
-if languages is None and action in ('add', 'delete'):
-    psm = 'psm_language_error_select_at_least_one_item'
-
-elif action == 'add':
-    # Make languages available in Localizer
-    for lang in languages:
-        for catalog in catalogs:
-            catalog.manage_addLanguage(lang)
-
-    # XXX needs a tools to register po files for domains
-    # Update Localizer/default only !
-    i18n_method = getattr(portal,'i18n Updater')
-    i18n_method()
-    psm = 'psm_language_added'
-
-elif action == 'delete':
-    # Make unavailable languages in Localizer
-    for catalog in catalogs:
-        catalog.manage_delLanguages(languages)
-    psm = 'psm_language_deleted'
-
-elif action == 'chooseDefault':
-    for catalog in catalogs:
-        catalog.manage_changeDefaultLang(lang)
-    psm = 'psm_default_language_set'
-
-else:
-    psm = ''
-
-if REQUEST is not None:
-    REQUEST.RESPONSE.redirect('%s/%s?portal_status_message=%s' % (context_url, template, psm))
+if request is not None:
+    request.RESPONSE.redirect('%s/%s?portal_status_message=%s' % (context_url, template, psm))
 return psm
+

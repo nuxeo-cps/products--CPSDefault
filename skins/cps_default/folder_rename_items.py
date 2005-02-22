67e5a2cc5f1c7a974547369786d32200818ce1dc
fguillaume <devnull@localhost>
2005-02-22 17:20:56 +0000

    Fixed object id change in folder, some skins were missing with CMF 1.5.0

---
+++ skins/cps_default/folder_rename_items.py
@@ -0,0 +1,22 @@
+##parameters=
+# $Id$
+"""Get the list of renameable objects from REQUEST's 'ids' list.
+"""
+
+# This is bad but we want to keep CMF 1.4's API for now
+REQUEST = context.REQUEST
+
+ids = REQUEST.get('ids', [])
+
+objects = []
+for id in ids:
+    if getattr(context.aq_explicit, id, None) is None:
+        continue
+    ob = getattr(context, id)
+    if not ob.cb_isMoveable():
+        continue
+    objects.append(ob)
+
+from zLOG import LOG, INFO
+LOG('folder_rename_items', INFO, str(objects))
+return objects
---
+++ skins/cps_default/iconHTML.py
@@ -0,0 +1,24 @@
+##parameters=
+# $Id$
+"""Returns the HTML for the current object's icon, if it is available.
+"""
+
+try:
+    url = context.getIcon()
+except (AttributeError, KeyError):
+    try:
+        url = context.icon
+    except (AttributeError, KeyError):
+        url = ''
+if callable(url):
+    url = url()
+
+if not url:
+    return ''
+
+try:
+    Type = context.Type()
+except (AttributeError, KeyError):
+    Type = ''
+
+return '<img src="%s" align="left" alt="%s" />' % (url, Type)

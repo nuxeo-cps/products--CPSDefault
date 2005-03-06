c8fb054e506ceda41a2cb8065149e31efe77173f
sfermigier <devnull@localhost>
2005-03-06 12:46:09 +0000

    Better default view (batch by 30 objects instead of 10).

--- skins/cps_default/getCustomDisplayParams.py
+++ skins/cps_default/getCustomDisplayParams.py
@@ -7,10 +7,9 @@ FIXME: docstring?
 return {'format': None,
         'sort_by': None,
         'direction': 'asc',
-        'items_per_page': 10,
+        'items_per_page': 30,
         'nav_action': 'folder_view',
         'filter': 0,
         'detail_tab_columns': ['title', 'type', 'size', 'date', 'author',
-                               'status', 'version',
-                                  ],
-           }
+                               'status', 'version'],
+       }

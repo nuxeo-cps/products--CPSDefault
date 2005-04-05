f89fb076a91a39cced96eacc9ace2cb2760b9f82
bdelbosc <devnull@localhost>
2005-04-05 09:52:15 +0000

    adding review state vocabulary

--- skins/cps_default_installer/getCPSDefaultFormVocabularies.py
+++ skins/cps_default_installer/getCPSDefaultFormVocabularies.py
@@ -3,6 +3,14 @@
 """Return CPSDefault forms vocabularies."""
 
 vocabularies = {
+    'review_state_voc': {
+        'type': 'CPS Vocabulary',
+        'data': {'tuples': (('work', 'Work', 'label_work'),
+                            ('pending', 'Pending', 'label_pending'),
+                            ('published', 'Published', 'label_published'),
+                            )},
+        },
+
     }
 
 return vocabularies

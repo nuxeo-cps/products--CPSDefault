e7805403798739b0e9e369206d7e26c1b00c2376
fguillaume <devnull@localhost>
2005-04-20 16:04:29 +0000

    Made folder_localrole_search and folder_localrole_form work with any id_field for the members directory.

--- skins/cps_default/folder_localrole_search.py
+++ skins/cps_default/folder_localrole_search.py
@@ -11,24 +11,25 @@ if not context.portal_membership.checkPermission('Change permissions', context):
 
 if search_param in ('fullname', 'email'):
     mdir = context.portal_directories.members
+    id_field = mdir.id_field
     # first get portal member ids without externall call (e.g. LDAP)
-    return_fields = ('id', 'givenName', 'sn', 'email')
+    return_fields = (id_field, 'givenName', 'sn', 'email')
     if search_param == 'fullname':
         # XXX cannot search both parameters at the same time because we want a
         # OR search, not AND.
-        from_ids = mdir.searchEntries(id=search_term,
-                                      return_fields=return_fields)
+        from_ids = mdir.searchEntries(**{id_field: search_term,
+                                         'return_fields': return_fields})
         from_fullnames = mdir.searchEntries(fullname=search_term,
                                             return_fields=return_fields)
         results = {}
         for id, values in (from_ids + from_fullnames):
             results[id] = values
         results = results.items()
-        results.sort()
-        return results
     elif search_param == 'email':
-        return mdir.searchEntries(email=search_term,
+        results = mdir.searchEntries(email=search_term,
                                      return_fields=return_fields)
+    results.sort()
+    return results
 elif search_param == 'groupname':
     gdir  = context.portal_directories.groups
     # XXX hardcoded but not GroupsDirectory's job

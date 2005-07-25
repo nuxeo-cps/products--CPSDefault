b6d695323a99e1a51015408a3db9e465efda9778
atchertchian <devnull@localhost>
2005-07-25 22:56:59 +0000

    bugfix #846: added backward compatibility code to make the getBaseUrl script work as expected just before the portal_url update

--- skins/cps_default/getBaseUrl.py
+++ skins/cps_default/getBaseUrl.py
@@ -15,6 +15,13 @@ from Products.CMFCore.utils import getToolByName
 if utool is None:
     utool = getToolByName(context, 'portal_url')
 
-base_url = utool.getBaseUrl()
+if hasattr(utool.aq_inner.aq_explicit, 'getBaseUrl'):
+    base_url = utool.getBaseUrl()
+else:
+    # backward compatibility
+    portal = utool.getPortalObject()
+    base_url = portal.absolute_url_path()
+    if not base_url.endswith('/'):
+        base_url += '/'
 
 return base_url

402fcf755e369d70693051b77fe9a5d583f6169e
atchertchian <devnull@localhost>
2005-07-28 17:23:09 +0000

    fix #846: Language selector does not work for cps with boxes

--- skins/cps_default/changeCPSLanguage.py
+++ skins/cps_default/changeCPSLanguage.py
@@ -1,12 +1,20 @@
-##parameters=lang
+##parameters=lang,REQUEST=None
 # $Id$
 """
 Switch UI locale to lang, reset documents language selection done by
 switchLanguage.
 """
+# XXX AT: resetSessionLanguageSelection and translation_service.changeLanguage
+# both manipulate the session. Is it possible to let only the translation
+# service do it?
 
 from Products.CPSCore.utils import resetSessionLanguageSelection
 
 resetSessionLanguageSelection(context.REQUEST)
 
-return context.translation_service.changeLanguage(lang=lang)
+context.translation_service.changeLanguage(lang=lang)
+
+if REQUEST is not None:
+    referer = REQUEST['HTTP_REFERER']
+    REQUEST.RESPONSE.redirect(referer)
+

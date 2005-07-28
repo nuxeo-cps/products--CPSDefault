d8ca3ce05bf9966676ec7f302e1f332df72ef85b
atchertchian <devnull@localhost>
2005-07-28 21:09:22 +0000

    refactored addtoFavorites script so that it does not break with virtual hosting. some issuies need o be fixed though (see bug #858)

--- skins/cps_default/addtoFavorites.py
+++ skins/cps_default/addtoFavorites.py
@@ -1,73 +1,78 @@
 ##parameters=REQUEST=None
 # $Id$
+"""Add a Link document in the favorites folder in homefolder, pointing towards
+current document
+
+Script checks that homefolder exists and that user can create content in it. If
+the favorites folder does not exists, it is created in homefolder. Script
+assumes that user can create content in its favorites folder.
+
+The script could check if a link towards current document is already in the
+favorites. This part of the code is commented out because it is costly.
 """
-FIXME: add docstring
-"""
 
-# Note: there is another solution that does not rely on HTTP_REFERER:
-# have the add_favorites action generate a url with the object to bookmark
-# as a parameter to the addtoFavorite script:
-# python:portal.portal_url() + '/addtoFavorites?link=' +
-# request.URL+'&object=' + object.absolute_url()
-# link and object are passed to this script and can be used respectively
-# to generate the bookmark URL and to retrieve the object (in order to get
-# its title and description).
+from Products.CMFCore.permissions import AddPortalContent
+from Products.CMFCore.utils import getToolByName
 
-# TODO: check that link doesn't exist already.
+utool = getToolByName(context, 'portal_url')
+mtool = getToolByName(context, 'portal_membership')
 
-from ZTUtils import make_query
+home_folder = mtool.getHomeFolder()
 
-portal = context.portal_url.getPortalObject()
-homeFolder = portal.portal_membership.getHomeFolder()
-
-favorites_id = 'Favorites'
-
-if favorites_id not in homeFolder.objectIds():
-    # try to i18n the title using UI locale,
-    # still better than just an english id
-    cpsmcat = context.translation_service
-    title = cpsmcat('action_view_favorites').encode('iso-8859-15', 'ignore')
-    homeFolder.invokeFactory('Workspace', favorites_id)
-    targetFolder = getattr(homeFolder, favorites_id)
-    targetFolder.getEditableContent().edit(proxy=targetFolder, Title=title)
-    context.portal_eventservice.notifyEvent('modify_object', targetFolder, {})
-
-targetFolder = getattr(homeFolder, favorites_id)
-
-new_id = 'fav_' + str(int(context.ZopeTime()))
-
-targetFolder.invokeFactory('Link', new_id)
-
-referer = REQUEST.HTTP_REFERER
-portal_URL_length = len(portal.portal_url())
-
-# Fallback in case HTTP_REFERER is empty.
-# Note: if referer is empty (or incorrect), the bookmark
-# might be incorrect - the above-mentioned method fixes this
-# but has not been retained because of its lower performace.
-if referer and len(referer) >= portal_URL_length:
-    referer_url = portal.portal_url.getPortalPath() + \
-        REQUEST.HTTP_REFERER[portal_URL_length:]
+if (home_folder is None
+    or not mtool.checkPermission(AddPortalContent, home_folder)):
+    # cannot add favorites
+    psm = 'psm_cannot_create_favorites'
 else:
-    referer_url = portal.portal_url.getPortalPath() + '/' + \
-        context.portal_url.getRelativeUrl(context)
+    favorites_id = 'Favorites'
 
-kw = {'Title': context.TitleOrId(),
-      'Description': context.getContent().description,
-      'Relation': referer_url}
+    # create favorites folder if it does not exist
+    if favorites_id not in home_folder.objectIds():
+        # try to i18n the title using UI locale,
+        # still better than just an english id
+        cpsmcat = context.translation_service
+        title = cpsmcat('action_view_favorites')
+        title.encode('iso-8859-15', 'ignore')
+        home_folder.invokeFactory('Workspace', favorites_id, Title=title)
+        fav_folder = getattr(home_folder, favorites_id)
 
-doc = getattr(targetFolder, new_id).getEditableContent()
+    fav_folder = getattr(home_folder, favorites_id)
 
-doc.edit(**kw)
+    # XXX AT: This link will break if the portal name changes for instance...
+    new_link = utool.getPortalPath() + '/' + utool.getRpath(context)
+
+    # check that the link does not already exists
+    link_exists = False
+    # XXX AT: commented because it could be costly
+    # Cannot query Relation as it is not indexed by default
+    #for fav in fav_folder.objectValues():
+    #    if getattr(fav, 'portal_type', None) == 'Link':
+    #        try:
+    #            fav_link = fav.getContent().Relation()
+    #        except AttributeError:
+    #            continue
+    #        if fav_link == new_link:
+    #            link_exists = True
+    #            break
+
+    if link_exists is True:
+        psm = 'psm_already_in_favorites'
+    else:
+        # get info about the document to set the link info
+        link_kws = {}
+        link_kws['Relation'] = new_link
+        doc = context.getContent()
+        try:
+            link_kws['Title'] = doc.title_or_id()
+            link_kws['Description'] = doc.Description()
+        except AttributeError:
+            link_kws['Title'] = context.getId()
+
+        new_id = 'fav_' + str(int(context.ZopeTime()))
+        fav_folder.invokeFactory('Link', new_id, **link_kws)
+        psm = 'psm_added_to_favorites'
 
 if REQUEST is not None:
-    if referer_url.count('portal_status_message'):
-        query = ''
-    else:
-        query = make_query(portal_status_message='psm_added_to_favorites')
-    if '?' in referer_url:
-        url = REQUEST.HTTP_SERVER + referer_url + '&' + query
-    else:
-        url = REQUEST.HTTP_SERVER + referer_url + '?' + query
+    url = context.absolute_url() + '?'
+    url += 'portal_status_message=' + psm
     return REQUEST.RESPONSE.redirect(url)
-

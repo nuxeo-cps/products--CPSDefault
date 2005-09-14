a8e636d119ee7f9242dae05f4c15523cea8d68a0
janguenot <devnull@localhost>
2005-09-14 17:43:10 +0000

    BBB

--- skins/cps_default/getMemberFullNameFromId.py
+++ skins/cps_default/getMemberFullNameFromId.py
@@ -4,14 +4,5 @@
 
 Lookup within all the members directories.
 """
-
-pdirectories=context.portal_directories
-directories=[pdirectories.members]
-
-for directory in directories:
-    try:
-        member = directory.getEntry(member_id)
-        return member.get('fullname', member_id)
-    except (KeyError, ValueError):
-        pass
-return member_id
+mtool = context.portal_membership
+return mtool.getFullnameFromId(member_id)

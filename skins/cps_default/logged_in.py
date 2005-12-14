5744dba436ab331e96b30a4f1143e427993ae3b3
madarche <devnull@localhost>
2005-12-14 11:43:07 +0000

    - Fixed #1205: Faulty logged_in redirect with a HTTPS secured site. - Also fixed a redirect that was using getBaseUrl() instead of an absolute URL   as required by the spec.

--- skins/cps_default/logged_in.py
+++ skins/cps_default/logged_in.py
@@ -4,13 +4,30 @@
 $Id$
 """
 
+from urllib import unquote
+
 utool = context.portal_url
 mtool = context.portal_membership
 dtool = context.portal_directories
+portal = utool.getPortalObject()
+portal_absolute_url = portal.absolute_url()
 
 redirect_url = came_from
+redirect_to_portal = False
+
 if not redirect_url or redirect_url.endswith('/logged_out'):
-    redirect_url = utool.getBaseUrl()
+    redirect_to_portal = True
+else:
+    redirect_url = unquote(redirect_url)
+    # One can be redirected from an http page while the login is done from an
+    # https page. This is a fix for #1205.
+    # A better option here would be to replace the previous portal_absolute_url
+    # prefix in the redirect_url by the current portal absolute URL.
+    if not redirect_url.startswith(portal_absolute_url):
+        redirect_to_portal = True
+
+if redirect_to_portal:
+    redirect_url = portal_absolute_url
 
 is_anon = mtool.isAnonymousUser()
 member = mtool.getAuthenticatedMember()
@@ -35,4 +52,5 @@ if first_time and dtool.members.hasEntry(member.getId()):
     mtool.createMemberArea()
     now = context.ZopeTime()
     member.setProperties(last_login_time=now, login_time=now)
+
 RESPONSE.redirect(redirect_url)

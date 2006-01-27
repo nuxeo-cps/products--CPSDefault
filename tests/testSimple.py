8578158411a41f715b7f4210eac94a1d9aeffa35
sfermigier <devnull@localhost>
2006-01-27 20:21:40 +0000

    Well, folder_factories is not really valid yet.

--- tests/testSimple.py
+++ tests/testSimple.py
@@ -66,10 +66,10 @@ class TestSimpleAsRoot(TestSimple):
         # Boxes
 
     def testAdminSkinsAtSectionsAndWorkspaces(self):
-        # FIXME: 'folder_edit_form', 'metadata_edit_form',
+        # FIXME: 'folder_edit_form', 'metadata_edit_form', 'folder_factories',
         # 'full_metadata_edit_form', 'folder_localrole_form' have validity
         # problems
-        view_ids = ('folder_view', 'folder_factories', 'folder_contents',)
+        view_ids = ('folder_view', 'folder_contents',)
         for folder_id in ('sections', 'workspaces', 'members'):
             folder = getattr(self.portal, folder_id)
             for view_id in view_ids:

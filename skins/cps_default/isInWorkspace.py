16da65524b5c1ce67a058ba384c50ef7bd6930f5
div <devnull@localhost>
2006-02-01 11:49:21 +0000

    update isInWorkspace script to take into account the Members Workspace portal type

--- skins/cps_default/isInWorkspace.py
+++ skins/cps_default/isInWorkspace.py
@@ -6,12 +6,13 @@ Return True if the context or the parameter 'object' is a workspace
 or inside a workspace.
 """
 
+ws = ['Workspace', 'Members Workspace']
 if not object:
     object = context
 
 try:
-    return object.portal_type == 'Workspace' \
-        or object.aq_parent.portal_type == 'Workspace'
+    return object.portal_type in ws \
+        or object.aq_parent.portal_type in ws
 except AttributeError:
     # FIXME: determine in which case it happens and how to react with the
     # portal's root

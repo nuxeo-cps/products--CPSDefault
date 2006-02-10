86c701bb776249aa29b2396c05a7eee7b29ef6a1
mturcu <devnull@localhost>
2006-02-10 13:35:19 +0000

    30 items are displayed per page; added <strong> element; added extra white space.

--- skins/cps_default/getCustomDisplayParams.py
+++ skins/cps_default/getCustomDisplayParams.py
@@ -7,7 +7,7 @@ if context.isInWorkspace():
     custom_params = {'format': 'detail_tab',
                      'sort_by': None,
                      'direction': 'asc',
-                     'items_per_page': 50,
+                     'items_per_page': 30,
                      'nav_action': 'folder_view',
                      'filter': 0,
                      'detail_tab_columns': ['title', 'type', 'size', 

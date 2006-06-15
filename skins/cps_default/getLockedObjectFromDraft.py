b628b06b48a4008686ce14012141fa63c053ca0a
ogrisel <devnull@localhost>
2006-06-15 14:29:49 +0000

    getLockedObjectFromDraft does not scan a complete folder (use the catalog instead)

--- skins/cps_default/getLockedObjectFromDraft.py
+++ skins/cps_default/getLockedObjectFromDraft.py
@@ -7,27 +7,29 @@ If reverse==1, find the draft from the locked object.
 Returns an object or None.
 """
 
-wftool = context.portal_workflow
-folder = context.aq_parent
+utool = context.portal_url
+folder = context.aq_inner.aq_parent
 docid = context.getDocid()
 flr = context.getFromLanguageRevisions()
+
 if not reverse:
     want_review_state = 'locked'
 else:
     want_review_state = 'draft'
-for ob in folder.objectValues():
-    try:
-        if ob.getDocid() != docid:
-            continue
-        if wftool.getInfoFor(ob, 'review_state', None) != want_review_state:
-            continue
-        if ob.getLanguageRevisions() != flr:
-            continue
-        return ob
-    except:
-        from zLOG import LOG
-        LOG('getLockedObjectFromDraft', 200,
-            'Exception in folder=%s' % folder)
-        raise
+
+# use a catalog query to avoid scanning the whole folder content
+query = {
+    'portal_type': context.portal_type,
+    'review_state': want_review_state,
+    'folder_prefix': utool.getRpath(folder),
+}
+
+for brain in context.search(query):
+    ob = brain.getObject()
+    if ob.getDocid() != docid:
+        continue
+    if ob.getLanguageRevisions() != flr:
+        continue
+    return ob
 
 return None

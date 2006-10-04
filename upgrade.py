7311a216c9b5ef6b6cb3e39b643b92964ccd0a98
madarche <devnull@localhost>
2006-10-04 14:49:09 +0000

    Cosmit.

--- upgrade.py
+++ upgrade.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2004 Nuxeo SARL <http://nuxeo.com>
+# Copyright (c) 2004-2006 Nuxeo SAS <http://nuxeo.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as published

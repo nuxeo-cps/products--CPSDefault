040976d5c7b76143cb9b16923ae5126de89d0b63
madarche <devnull@localhost>
2008-09-10 09:22:12 +0000

    Fixed #1917: Improve the HTML title of pages so it is more meaningful

--- skins/cps_default/expanded_title.py
+++ skins/cps_default/expanded_title.py
@@ -1,21 +1,30 @@
 ## Script (Python) "expanded_title"
 ##parameters=
-##title=Build title which includes site title
+##title=Build title which includes site title, page title and site description
 ##
 # $Id$
 #
 # Fix for #1895: UnicodeDecodeError when viewing a calendar if CPS site title
 # has accented characters.
-# BBB: Remove this customized script when the switch to UTF8 is done.
+#
+# TODO: Depending on what CMF does, maybe remove this customized script when the
+# switch to UTF8 is done.
 
 from Products.CPSUtil.text import toLatin9
 
 site_title = context.portal_url.getPortalObject().Title()
+site_description = context.portal_url.getPortalObject().Description()
 page_title = context.Title()
 page_title = toLatin9(page_title)
 
-if page_title != site_title:
-   page_title = site_title + ": " + page_title
+if page_title == site_title:
+    # This is portal root (homepage) or a page not corresponding to a document
+    expanded_title = site_title + ': ' + site_description
+else:
+    # Here it's more meaningful to put the title after the title
+    # of the page especially when the whole title is long
+    # and the page is bookmarked.
+    expanded_title = page_title + ' | ' + site_title
 
-return page_title
+return expanded_title
 

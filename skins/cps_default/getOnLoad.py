0a86bd9550a49d9fd8406601bd2bab2d1fe548a1
M.-A. Darche <ma.darche@aful.org>
2010-05-27 16:07:48 +0200

    Easier to customize

--- skins/cps_default/getOnLoad.py
+++ skins/cps_default/getOnLoad.py
@@ -2,9 +2,12 @@
 """
 Returns the JavaScript code to put in the HTML body "onload" attribute.
 """
+
+javascript = 'setFocus();'
+
 URL = container.REQUEST.get('URL')
 if URL is not None and \
     (URL.endswith('search_form') or URL.endswith('advanced_search_form')):
-    return 'highlightSearchTerm(); setFocus();'
+    return 'highlightSearchTerm(); ' + javascript
 
-return 'setFocus();'
+return javascript

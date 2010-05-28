111408d3945e806e202fd0a7f30011011c712a16
M.-A. Darche <ma.darche@aful.org>
2010-05-28 09:03:03 +0200

    The message information notification should be active only for logged in users

--- skins/cps_default/getOnLoad.py
+++ skins/cps_default/getOnLoad.py
@@ -4,12 +4,16 @@ Returns the JavaScript code to put in the HTML body "onload" attribute.
 """
 
 portal = context.portal_url.getPortalObject()
+mtool = portal.portal_membership
+is_anon = mtool.isAnonymousUser()
+javascript = ""
+if not is_anon:
+    javascript = """
+    org.cps_cms.InformationMessageFetcher.portal_url = '%s';
+    org.cps_cms.InformationMessageFetcher.continuousCheckForInformationMessage();
+    """ % portal.absolute_url()
 
-javascript = """
-org.cps_cms.InformationMessageFetcher.portal_url = '%s';
-org.cps_cms.InformationMessageFetcher.continuousCheckForInformationMessage();
-setFocus();
-""" % portal.absolute_url()
+javascript += "setFocus();"
 
 URL = container.REQUEST.get('URL')
 if URL is not None and \

794bb4100d770920b5c61291f3f5e456939737fe
M.-A. Darche <ma.darche@aful.org>
2010-06-03 14:22:58 +0200

    Removed obsolete files

--- skins/cps_default/getCPSLocalRoles.py
+++
@@ -1,8 +0,0 @@
-##parameters=cps_roles=None
-# $Id$
-"""
-XXX content moved into portal_membership
-"""
-from Products.CMFCore.utils import getToolByName
-mtool = getToolByName(context, 'portal_membership')
-return mtool.getCPSLocalRoles(context, cps_roles)

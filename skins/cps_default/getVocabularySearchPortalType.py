97dd2d0df0afdff42f56212e6d94da59c4cc2b1c
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-10-14 07:36:58 +0200

    #2264: fixed search type voc and leftover in folder_factories

--- skins/cps_default/getVocabularySearchPortalType.py
+++ skins/cps_default/getVocabularySearchPortalType.py
@@ -3,9 +3,8 @@
 """Return a portal type vocabulary, used as MethodVocabulary."""
 
 types = context.getSortedContentTypes(allowed=0)
-l10n = context.translation_service
-res = [(item.getId(), l10n(item.Title()))
-       for item in types]
+res = [(item['id'], item['Title']) for item in types]
+
 if key is not None:
     res = [item[1] for item in res if item[0] == key][0]
 

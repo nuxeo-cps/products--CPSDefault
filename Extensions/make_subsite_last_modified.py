42b71322c193c58fab0b0601418b47a57395b6ad
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-12-22 20:12:10 +0100

    #2506: external method sets to current date/time in pre-existing or new cases

--- Extensions/make_subsite_last_modified.py
+++ Extensions/make_subsite_last_modified.py
@@ -1,6 +1,11 @@
+from DateTime.DateTime import DateTime
 from Products.CPSUtil.conflictresolvers import IncreasingDateTime
 from Products.CPSDefault.voidresponses import SUBSITE_LAST_MODIFIED_DATE
 
 def make(self):
-    lmd = IncreasingDateTime(SUBSITE_LAST_MODIFIED_DATE)
-    self._setObject(SUBSITE_LAST_MODIFIED_DATE, lmd)
+    if self.hasObject(SUBSITE_LAST_MODIFIED_DATE):
+        lmd = self[SUBSITE_LAST_MODIFIED_DATE]
+    else:
+        lmd = IncreasingDateTime(SUBSITE_LAST_MODIFIED_DATE)
+        self._setObject(SUBSITE_LAST_MODIFIED_DATE, lmd)
+    lmd.set(DateTime())
